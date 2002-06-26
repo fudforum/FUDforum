639623ab9653eddbcb0ab252e2208d668c3892c4
iliaa <ilia@ilia.ws>
2002-06-26 19:49:29 +0000

    Moved into mysql

---
+++ install/forum_data/sql/mysql/def_groups.sql
@@ -0,0 +1,2 @@
+INSERT INTO {SQL_TABLE_PREFIX}groups (id, name, inherit_id, joinmode, res, res_id, p_VISIBLE, p_READ) values(1, 'Global Anonymous Access', 0, 'NONE', 'global', 0, 'Y', 'Y');
+INSERT INTO {SQL_TABLE_PREFIX}groups (id, name, inherit_id, joinmode, res, res_id, p_VISIBLE, p_READ, p_POST, p_REPLY, p_POLL, p_FILE, p_VOTE, p_RATE, p_SML, p_IMG) values(2, 'Global Registered Access', 0, 'NONE', 'global', 0, 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
