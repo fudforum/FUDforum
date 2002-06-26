0376ed816957ea05025d0479eb33c8793988eb51
iliaa <ilia@ilia.ws>
2002-06-26 23:56:45 +0000

    Fixed the seq sets

--- install/forum_data/sql/pgsql/def_groups.sql
+++ install/forum_data/sql/pgsql/def_groups.sql
@@ -1,3 +1,4 @@
 INSERT INTO {SQL_TABLE_PREFIX}groups (id, name, inherit_id, joinmode, res, res_id, p_VISIBLE, p_READ) values(1, 'Global Anonymous Access', 0, 'NONE', 'global', 0, 'Y', 'Y');
 INSERT INTO {SQL_TABLE_PREFIX}groups (id, name, inherit_id, joinmode, res, res_id, p_VISIBLE, p_READ, p_POST, p_REPLY, p_POLL, p_FILE, p_VOTE, p_RATE, p_SML, p_IMG) values(2, 'Global Registered Access', 0, 'NONE', 'global', 0, 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
-SELECT setval('{SQL_TABLE_PREFIX}groups_id_seq', 2);
+INSERT INTO {SQL_TABLE_PREFIX}groups (id, name, res, res_id, p_VISIBLE, p_READ, p_POST, p_REPLY, p_EDIT, p_DEL, p_STICKY, p_POLL, p_FILE, p_VOTE, p_RATE, p_SPLIT, p_LOCK, p_MOVE, p_SML, p_IMG) VALUES (3, 'TestForum', 'forum', 1,  'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+SELECT setval('{SQL_TABLE_PREFIX}groups_id_seq', 3);
