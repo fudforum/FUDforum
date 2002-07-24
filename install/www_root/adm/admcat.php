40acb3abde9b157a261bc88aae8fb51fcfa538ab
iliaa <ilia@ilia.ws>
2002-07-24 14:44:12 +0000

    Fixed cutting of the description if it lacks default ' - ' prefix

--- install/www_root/adm/admcat.php
+++ install/www_root/adm/admcat.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admcat.php,v 1.3 2002/06/26 19:41:21 hackie Exp $
+*   $Id: admcat.php,v 1.4 2002/07/24 14:44:12 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -50,7 +50,7 @@
 		exit();
 	}
 	else if ( !empty($edit) ) {
-		if ( strlen($cat->description) ) $cat->description = substr($cat->description, 3);
+		if ( strlen($cat->description) ) $cat->description = preg_replace('!\s*-\s*!', '', $cat->description);
 		$cat->export_vars('cat_');
 	}
 	
