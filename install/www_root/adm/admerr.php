1a973cbf5abd4e45556f719e2305df940b461f9a
iliaa <ilia@ilia.ws>
2002-08-29 14:36:26 +0000

    Code cleanup

--- install/www_root/adm/admerr.php
+++ install/www_root/adm/admerr.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admerr.php,v 1.1 2002/08/28 18:39:27 hackie Exp $
+*   $Id: admerr.php,v 1.2 2002/08/29 14:36:26 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -43,8 +43,8 @@
 
 <?php
 	if( @file_exists($GLOBALS['ERROR_PATH'].'fud_errors') && filesize($GLOBALS['ERROR_PATH'].'fud_errors') ) {
-		echo '<h4>FUDforum Error Log [<a href="admerr.php?clear_fud_log=1&<? echo _rsid; ?>">clean log</a>]</h4>';
-		echo '<table border=1 cellspacing=1 cellpadding=3><tr bgcolor="#bff8ff"><td nowrap>Time (<b>GMT</b>)</td><td>Error Description</td></tr>';
+		echo '<h4>FUDforum Error Log [<a href="admerr.php?clear_fud_log=1&<? echo _rsid; ?>">clear log</a>]</h4>';
+		echo '<table border=1 cellspacing=1 cellpadding=3><tr bgcolor="#bff8ff"><td>Time</td><td>Error Description</td></tr>';
 		
 		$errors = file($GLOBALS['ERROR_PATH'].'fud_errors');
 		foreach( $errors as $error ) {
@@ -55,8 +55,8 @@
 	}
 
 	if( @file_exists($GLOBALS['ERROR_PATH'].'sql_errors') && filesize($GLOBALS['ERROR_PATH'].'sql_errors') ) {
-		echo '<h4>SQL Error Log [<a href="admerr.php?clear_sql_log=1&<? echo _rsid; ?>">clean log</a>]</h4>';
-		echo '<table border=1 cellspacing=1 cellpadding=3><tr bgcolor="#bff8ff"><td nowrap>Time (<b>GMT</b>)</td><td>Error Description</td></tr>';
+		echo '<h4>SQL Error Log [<a href="admerr.php?clear_sql_log=1&<? echo _rsid; ?>">clear log</a>]</h4>';
+		echo '<table border=1 cellspacing=1 cellpadding=3><tr bgcolor="#bff8ff"><td>Time</td><td>Error Description</td></tr>';
 		
 		$errors = file($GLOBALS['ERROR_PATH'].'sql_errors');
 		foreach( $errors as $error ) {
@@ -66,6 +66,8 @@
 		echo '</table><br /><br />';
 	}
 
+	if( !isset($errors) ) 
+		echo "<h4>Error logs are currently empty</h4><br />";
 
 	require('admclose.html'); 
 ?>
