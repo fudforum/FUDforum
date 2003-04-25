2ed66b57df8a6c2fb58557b6c175bfd5d266cd18
iliaa <ilia@ilia.ws>
2003-04-25 13:01:04 +0000

    Initial fixes.

--- install/www_root/adm/msglist.php
+++ install/www_root/adm/msglist.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: msglist.php,v 1.9 2002/09/18 20:52:08 hackie Exp $
+*   $Id: msglist.php,v 1.10 2003/04/25 13:01:04 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -18,120 +18,117 @@
 	@set_time_limit(6000);
 
 	define('admin_form', 1);
-	
-	include_once "GLOBALS.php";
-	
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
 	
-	list($ses, $usr) = initadm();
-	
-	if ( empty($tname) || empty($tlang) ) {
-		header("Location: admthemesel.php?ret=msglist&"._rsidl."&rand=".get_random_value());
-		exit();
+	$tname = isset($_POST['tname']) ? $_POST['tname'] : (isset($_GET['tname']) ? $_GET['tname'] : '');
+	$tlang = isset($_POST['tlang']) ? $_POST['tlang'] : (isset($_GET['tlang']) ? $_GET['tlang'] : '');
+
+	if (!$tname || !$tlang) {
+		header('Location: admthemesel.php?ret=msglist&'._rsidl);
+		exit;
 	}
 
-$msgfile = $GLOBALS['DATA_DIR'].'thm/'.$tname.'/i18n/'.$tlang.'/msg';
-if ( !@is_file($msgfile) ) {
-	$msgfile = $GLOBALS['DATA_DIR'].'thm/default/i18n/'.$tlang.'/msg';
-	$warn = 1;
-}
-
-function tmpllist_resolve_refernce($refs, &$file)
-{
-	global $deps;
-	
-	foreach($refs as $k => $v) { 
-		if( is_array($deps[$k]) ) tmpllist_resolve_refernce($deps[$k], $file);
-		$file[$k] = $v;
+	$msgfile = $GLOBALS['DATA_DIR'].'thm/'.$tname.'/i18n/'.$tlang.'/msg';
+	if (!@file_exists($msgfile)) {
+		$msgfile = $GLOBALS['DATA_DIR'].'thm/default/i18n/'.$tlang.'/msg';
+		$warn = 1;
 	}
-}
 
 function makedeps()
 {
-	$oldcwd = getcwd();
-	chdir($GLOBALS['DATA_DIR'].'thm/'.$GLOBALS['tname'].'/tmpl');
-	$dp = opendir('.');
+	$path = $GLOBALS['DATA_DIR'].'thm/'.$GLOBALS['tname'].'/tmpl';
+	$dp = opendir($path);
 	readdir($dp); readdir($dp);
 	while( $file = readdir($dp) ) {
-		if( substr($file, -5) == '.tmpl' ) {
-			$data = filetomem($file);
+		if (substr($file, -5) == '.tmpl') {
+			$data = file_get_contents($path . '/' . $file);
 
 			// check for msgs int the php code
-			$s=$e=0;
-			
-			while( ($s = strpos($data, '{REF: ', $s)) !== false ) {
+			$s = $e = 0;
+
+			while (($s = strpos($data, '{REF: ', $s)) !== false) {
 				$s += 6;
-				if( ($e=strpos($data, '}', $s)) === false ) break;
-				
-				$dep = trim(substr($data, $s, ($e-$s)));
-				if( !isset($deps[$file][$dep]) ) $deps[$file][$dep] = $dep;
+				if (($e=strpos($data, '}', $s)) === false) {
+					break;
+				}
+
+				$dep = substr($data, $s, ($e - $s));
+				if (!isset($deps[$file][$dep])) {
+					$deps[$file][$dep] = $dep;
+				}
 				$s = $e;
 			}
 			
-			while( ($s = strpos($data, '{MSG: ', $s)) !== false ) {
+			while (($s = strpos($data, '{MSG: ', $s)) !== false) {
 				$s += 6;
-				if( ($e=strpos($data, '}', $s)) === false ) break;
+				if (($e=strpos($data, '}', $s)) === false) {
+					break;
+				}
 				
-				$msg = trim(substr($data, $s, ($e-$s)));
-				if( !isset($tmplmsglist[$file][$msg]) ) $tmplmsglist[$file][$msg] = $msg;
+				$msg = substr($data, $s, ($e - $s));
+				if (!isset($tmplmsglist[$file][$msg])) {
+					$tmplmsglist[$file][$msg] = $msg;
+				}
 				$s = $e;
 			}
 		}
 	}
-	chdir($oldcwd);
 	
 	// build reverse deps
 	foreach($deps as $file => $reflist) {
-		foreach($reflist as $depfile) $filedeps[$depfile][] = $file;
+		foreach($reflist as $depfile) {
+			$filedeps[$depfile][] = $file;
+		}
 	}
-	
-	$v[0] = $tmplmsglist;
-	$v[1] = $filedeps;
-	
-	return $v;
+
+	return array($tmplmsglist, $filedeps);
 }
 
-	if ( $btn_submit ) {
-		$data = filetomem($msgfile);
-		preg_match_all('/(.+?):(\s*)(.+?)\n/s', $data, $regs, PREG_SET_ORDER);
-		$fdata = '';
-		foreach($regs as $v) { 
-			$nval = str_replace("\n", '\n', stripslashes($HTTP_POST_VARS[$v[1]]));
-			if ( isset($HTTP_POST_VARS[$v[1]]) && $v[3] != $nval ) {
-				if ( !isset($clist[$v[1]]) ) $clist[$v[1]] = 1;
-				$fdata .= $v[1].':'.$v[2].$nval."\n";
-			}
-			else 
-				$fdata .= $v[0];
+	if (isset($_POST['btn_submit'], $_POST['msg_list'])) {
+		$msglist_arr[] = strtok($_POST['msg_list'], ':');
+		while (($v = strtok(':'))) {
+			$msglist_arr[] = $v;			
 		}
-		$fp = fopen($msgfile, 'wb');
-		fwrite($fp, str_replace("\r", "", $fdata));
-		fclose($fp);
-		@chmod($msgfile, 0600);
-		
-		/* compile stuff here */
-		
-		list($tmplmsglist, $filedeps) = makedeps();
-		/* build msg->file */
-		foreach($tmplmsglist as $k => $v) {
-			foreach($v as $v2) {
-				if ( !isset($msgdeps[$v2][$k]) ) $msgdeps[$v2][$k] = $k;
-			}
-		}
-		
-		fud_use('compiler.inc', true);
-		$r = q("SELECT * FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."themes WHERE theme='$tname' AND lang='$tlang'");
-		while ( $obj = db_rowobj($r) )
-			compile_all($obj->theme, $obj->lang, $obj->name);
 
+		$data = file_get_contents($msgfile);
+		foreach ($msglist_arr as $v) {
+			if (($s = strpos($data, $v)) === FALSE) {
+				continue;
+			}
+			$s += 2 + strlen($v);
+			while ($data[$s] == "\t") {
+				++$s;
+			}
+			if (($e = strpos($data, "\n", $s)) === FALSE) {
+				continue;
+			}
+			$data = substr_replace($data, $_POST[$v], $s, ($e - $s));
+		}
+		if (!($fp = fopen($msgfile, 'wb'))) {
+			exit('unable to write to "'.$msgfile.'" message file');
+		}
+		fwrite($fp, $data);
+		fclose($fp);
+		fud_use('compiler.inc', true);
+
+		$c = q('SELECT theme FROM '.$GLOBALS['DBHOST_TBL_PREFIX'].'themes WHERE theme=\''.addslashes($tname).'\' AND lang=\''.addslashes($tlang).'\'');
+		while ($r = db_rowarr($c)) {
+			compile_all($tname, $tlang, $r[0]);
+		}
+		qf($c);
+
+		if (isset($_POST['NO_TREE_LIST'])) {
+			exit('<html><script>window.close();</script></html>');
+		}
 		exit('<br><a href="msglist.php?tname='.$tname.'&tlang='.$tlang.'&'._rsid.'">Back to control panel</a>');
-		if( $NO_TREE_LIST ) exit('<html><script>window.close();</script></html>');
 	}
 	
-if ( empty($NO_TREE_LIST) ) {
+if (!isset($_GET['NO_TREE_LIST'])) {
 	list($tmplmsglist, $filedeps) = makedeps();
+	ksort($tmplmsglist);
 
-	include('admpanel.php');
+	require($WWW_ROOT_DISK . 'adm/admpanel.php');
 ?>
 <style>
 .file_name {
@@ -153,28 +150,26 @@ if ( empty($NO_TREE_LIST) ) {
 }
 </style>
 <table border=0 cellspacing=0 cellpadding=0><tr><td valign=top>
-
 <table border=0 cellspacing=0 cellpadding=3>
 <?php
-	ksort($tmplmsglist);
 
-if ( $warn ) {
+if (isset($warn)) {
 	echo '<div align="center"><font color="green" size="+2">WARNING: EDITING DEFAULT MESSAGE FILE, BECAUSE THIS TEMPLATE DOESN\'T HAVE ONE</font><br><br></div>';
 }
+	$tab = str_repeat('&nbsp;', 5);
 
 	foreach($tmplmsglist as $file => $msg) { 
 		$list = $msgnamelist = '';
 		foreach($msg as $k => $msgname) { 
 			$msgnamelist .= urlencode($msgname).':';
-			$list .='<tr><td><img src="blank.gif" height=1 width=20><a class="deps" href="msglist.php?tname='.$tname.'&tlang='.$tlang.'&'._rsid.'&msglist='.urlencode($msgname).'&fl='.$file.'">'.$msgname."</a></td></tr>\n";
+			$list .='<tr><td><img src="blank.gif" height=1 width=20><a class="deps" href="msglist.php?tname='.$tname.'&tlang='.$tlang.'&'._rsidl.'&msglist='.urlencode($msgname).'&fl='.$file.'">'.$msgname.'</a></td></tr>';
 		}
 		$msgnamelist = substr($msgnamelist, 0, -1);
-		echo "\t".'<tr><td><a class="file_name" href="msglist.php?tname='.$tname.'&tlang='.$tlang.'&'._rsid.'&msglist='.$msgnamelist.'&fl='.$file.'">'.$file.'</a><a name="'.$file.'"></a></td></tr>'."\n";
-		echo $list;
-		if ( count($filedeps[$file]) ) {
-			echo '<tr><td class="depson"><img src="blank.gif" height=1 width=20><b>&raquo; Used By:</b></td></tr>'."\n";
+		echo '<tr><td><a class="file_name" href="msglist.php?tname='.$tname.'&tlang='.$tlang.'&'._rsidl.'&msglist='.$msgnamelist.'&fl='.$file.'">'.$file.'</a><a name="'.$file.'"></a></td></tr>' . $list;
+		if (isset($filedeps[$file])) {
+			echo '<tr><td class="depson">'.$tab.'<b>&raquo; Used By:</b></td></tr>'."\n";
 			foreach($filedeps[$file] as $v) { 
-				echo "\t\t".'<tr><td><img src="blank.gif" height=1 width=40><a href="#'.$v.'" class="depson">'.$v."</a></td></tr>\n";
+				echo '<tr><td>'.$tab.$tab.'<a href="#'.$v.'" class="depson">'.$v.'</a></td></tr>';
 			}
 		}
 		
@@ -182,43 +177,46 @@ if ( $warn ) {
 	echo '</table></td>';
 } /* NO_TREE_LIST */ 
 
+	$msglist = isset($_GET['msglist']) ? $_GET['msglist'] : (isset($_POST['msglist']) ? $_POST['msglist'] : '');
 
-if ( $MSG_LIST || !empty($msglist) ) {
-	echo '<td valign=top><form method="post" action="msglist.php?tname='.$tname.'&tlang='.$tlang.'"><table border=0>'._hs;
-	
-	$msglist = $HTTP_GET_VARS['msglist'];
-	if ( !$msglist ) $msglist = $HTTP_POST_VARS['msglist'];		
-	if ( $msglist ) {
-		$msglist_tmp = explode(':', $msglist);
-		foreach($msglist_tmp as $v) $msglist_arr[$v] = 1;
-	}
-
-	$data = filetomem($msgfile);
-
-	preg_match_all('/(.+?):(\s*?)(.+?)\n/s', $data, $regs, PREG_SET_ORDER);
-	foreach($regs as $v) {
-		if ( isset($msglist_arr) && !isset($msglist_arr[$v[1]]) ) continue;
-		$v[2] = trim($v[3]);
-		if ( ($ln=strlen($v[2])) > 50 ) {
-			$cols = 50;
-			$rows = $ln/$cols;
-			$rows += 2;
-			$maxrows = 20;
-			if ( $rows > $maxrows ) $rows = $maxrows;
-			$inptd = "<textarea name=\"$v[1]\" rows=$rows cols=$cols>".htmlspecialchars($v[2])."</textarea>";
+	if ($msglist) {
+		echo '<td valign=top><form method="post" action="msglist.php?tname='.$tname.'&tlang='.$tlang.'"><table border=0>'._hs;
+		$msglist_arr[] = strtok($msglist, ':');
+		while (($v = strtok(':'))) {
+			$msglist_arr[] = $v;			
 		}
-		else 
-			$inptd = "<input type=\"text\" name=\"$v[1]\" value=\"".htmlspecialchars($v[2])."\" size=50>";
-		echo "<tr><td valign=top nowrap><a name=\"$v[1]\"><b>$v[1]</b></a>:</td><td valign=top>$inptd</td></tr>\n";
-	}
-	?>
+
+		$data = file_get_contents($msgfile);
+		
+		foreach ($msglist_arr as $v) {
+			if (($s = strpos($data, $v)) === FALSE) {
+				continue;
+			}
+			$s += 2 + strlen($v);
+			if (($e = strpos($data, "\n", $s)) === FALSE) {
+				continue;
+			}
+			$txt = htmlspecialchars(trim(substr($data, $s, ($e - $s))));
+			if (strlen($txt) > 50) {
+				$rows = strlen($txt) / 50 + 2;
+				if ($rows > 20) {
+					$rows = 20;
+				}
+
+				$inptd = '<textarea name="'.$v.'" rows='.$rows.' cols=50>'.$txt.'</textarea>';
+			} else {
+				$inptd = '<input type="text" name="'.$v.'" value="'.$txt.'" size=50>';
+			}
+			echo '<tr><td valign=top nowrap><a name="'.$v.'"><b>'.$v.'</b></a>:</td><td valign=top>'.$inptd.'</td></tr>';
+		}
+?>
 	<tr><td align=right colspan=2><input type="submit" name="btn_submit" value="Edit"></td></tr>
 	<input type="hidden" name="msglist" value="<?php echo $msglist; ?>">
 	<input type="hidden" name="fl" value="<?php echo $fl; ?>">
 	</table></td>
 	</form>
-	<?php
-}
+<?php
+	}
 ?>
 </tr></table>
-<?php readfile('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
