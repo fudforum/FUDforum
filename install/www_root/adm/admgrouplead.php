90f906f489cae06adf318f692ec7df74a02a4e17
iliaa <ilia@ilia.ws>
2003-04-25 20:15:33 +0000

    Fixes.

--- install/www_root/adm/admgrouplead.php
+++ install/www_root/adm/admgrouplead.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admgrouplead.php,v 1.9 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admgrouplead.php,v 1.10 2003/04/25 20:15:33 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,69 +17,55 @@
 
 	define('admin_form', 1);
 
-	include_once "GLOBALS.php";
-	
-	fud_use('db.inc');
-	fud_use('cookies.inc');
-	fud_use('time.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('users.inc');
-	fud_use('users_reg.inc');
-	fud_use('widgets.inc', true);
 	fud_use('groups.inc');
-	fud_use('is_perms.inc');
-	fud_use('rev_fmt.inc');
+	fud_use('groups_adm.inc', true);
 
-	list($ses, $adm) = initadm();
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
 
-	if ( empty($group_id) ) {
-		header("Location: admgroups.php?rnd=".get_random_value()."&"._rsidl);
-		exit();
+	$group_id = isset($_GET['group_id']) ? (int)$_GET['group_id'] : (isset($_POST['group_id']) ? (int)$_POST['group_id'] : '');
+	$gr_leader = isset($_GET['gr_leader']) ? $_GET['gr_leader'] : (isset($_POST['gr_leader']) ? $_POST['gr_leader'] : '');
+	
+	if (!$group_id) {
+		header('Location: admgroups.php?'._rsidl);
+		exit;
 	}	
 
-	$grp = new fud_group;
+	if (isset($_GET['del'])) {
+		q('DELETE FROM '.$tbl.'group_members WHERE user_id='.(int)$_GET['del']);
+		q('DELETE FROM '.$tbl.'group_cache WHERE user_id='.(int)$_GET['del']);
+	} else if ($gr_leader) {
+		$srch = addslashes(str_replace('\\', '\\\\', htmlspecialchars($gr_leader)));
 
-	if ( !empty($gr_leader) ) {
-		$grp->get($group_id);
-		$usr = new fud_user;
-		if ( !($usr_id = get_id_by_alias($gr_leader)) ) {
-			
-			$r = q("SELECT alias FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE alias LIKE '".strtolower(addslashes(str_replace('\\', '\\\\', stripslashes($gr_leader))))."%' LIMIT 100");
-			
-			if ( db_count($r) ) {
-				echo "<html>
-					".stripslashes($gr_leader)." isn't found, perhaps you mean one of these?<br>
-					<table border=0 cellspacing=0 cellpadding=3>
-					";
-				while ( $obj = db_rowobj($r) ) {
-					reverse_FMT($obj->alias);
-					echo "<tr><td><a href=\"admgrouplead.php?gr_leader=".urlencode($obj->alias)."&group_id=$group_id&"._rsid."\">$obj->alias</a></td></tr>";
+		$c = q('SELECT id, alias FROM '.$tbl.'users WHERE alias LIKE \''.$srch.'%\' LIMIT 50');
+		switch (($cnt = db_count($c))) {
+			case 0:
+				$error = 'Could not find a user who matches the "'.$srch.'" login mask';
+				break;
+			case 1:
+				$r = db_rowarr($c);
+				$flds = implode(',', $GLOBALS['__GROUPS_INC']['permlist']);
+				q('INSERT INTO '.$tbl.'group_members ('.str_replace('p_', 'up_', $flds).', group_leader, group_id, user_id) SELECT '.$flds.', \'Y\', id, '.$r[0].' FROM '.$tbl.'groups WHERE id='.$group_id);
+				grp_rebuild_cache($group_id, $r[0]);
+				$gr_leader = '';
+				break;
+			default:
+				/* more then 1 user found, draw a selection form */
+				echo '<html><body bgcolor="#ffffff">There are '.$cnt.' users matching your search mask:<br><table border=0 cellspacing=0 cellpadding=3>';
+				while ($r = db_rowarr($c)) {
+					echo '<tr><td><a href="admgrouplead.php?gr_leader='.urlencode($r[1]).'&group_id='.$group_id.'&'._rsidl.'">'.$r[1].'</a></td></tr>';
 				}
-				echo "</table>";
-			}
-			else 
-				echo "Couldn't match any users to your query<br><a href=\"admgrouplead.php?group_id=$group_id&rnd=".get_random_value()."&"._rsid."\">Back</a>\n";
-			
-			exit();
-		}		
-		$usr->get_user_by_id($usr_id);
-
-		$grp->add_leader($usr->id);
-		$grp->rebuild_cache($usr->id);
-		header("Location: admgrouplead.php?group_id=$group_id&rnd=".get_random_value()."&"._rsidl);
-		exit();
+				qf($c);
+				echo '</table></body></html>';
+				exit;
+		}
+		qf($c);
 	}
-	$grp->get($group_id);
 
-	if ( $del ) {
-		$grp->delete_member($del);
-		$grp->rebuild_cache($del);
-		header("Location: admgrouplead.php?group_id=$group_id&rnd=".get_random_value()."&"._rsidl);
-		exit();
-	}
-include('admpanel.php'); 
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
-<a href="admgroups.php">Back to Groups</a>
+<a href="admgroups.php?<?php echo _rsidl; ?>">Back to Groups</a>
 <form method="post" action="admgrouplead.php"><?php echo _hs; ?>
 <input type="hidden" value="<?php echo $group_id; ?>" name="group_id">
 <table border=0 cellspacing=0 cellpadding=3>
@@ -90,12 +76,12 @@ include('admpanel.php');
 <table border=1 cellspacing=1 cellpadding=3>
 <tr><td>Leader Login</td><td>Action</td></tr>
 <?php
-	$llist = $grp->get_leader_list();
-	if( is_array($llist) ) {
-		foreach($llist as $v)
-			echo "<tr><td>$v->alias</td><td>[<a href=\"admgrouplead.php?group_id=$group_id&del=$v->user_id&"._rsid."\">Remove From Group</a>]</tr>\n";
-	}	
+	$c = uq('SELECT u.id, u.alias FROM '.$tbl.'group_members gm INNER JOIN '.$tbl.'users u ON u.id=gm.user_id WHERE gm.group_id='.$group_id);
+	while ($r = db_rowarr($c)) {
+		echo '<tr><td>'.$r[1].'</td><td>[<a href="admgrouplead.php?group_id='.$group_id.'&del='.$r[0].'&'._rsidl.'">Remove From Group</a>]</td></tr>';
+	}
+	qf($c);
 ?>
 </table>
 </form>
-<?php require('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
