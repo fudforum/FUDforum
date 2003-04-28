c4de01b739a114df7e8aab9c41f136ef3d799919
iliaa <ilia@ilia.ws>
2003-04-28 14:12:23 +0000

    Fixes.

--- install/www_root/adm/admavatar.php
+++ install/www_root/adm/admavatar.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admavatar.php,v 1.4 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admavatar.php,v 1.5 2003/04/28 14:12:23 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,96 +17,65 @@
 
 	define('admin_form', 1);
 
-	include_once "GLOBALS.php";
-	
-	fud_use('widgets.inc', true);
-	fud_use('util.inc');
-	fud_use('avatar.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	
-	list($ses, $usr) = initadm();
-	
-	$avatar_dir = '../images/avatars/';
-	
-	cache_buster();
-	
-	if ( !empty($btn_cancel) ) {
-		header("Location: admavatar.php?"._rsidl);
-	}
-	
-	if ( !empty($del) ) {
-		$avt_d = new fud_avatar;
-		$avt_d->get($del);
-		$avt_d->delete();
-		header("Location: admavatar.php?"._rsidl);
-		exit();
-	}
-	
-	if ( !empty($btn_update) && !empty($edit) ) {
-		$avt_u = new fud_avatar;
-		$avt_u->get($edit);
-		$avt_u->fetch_vars($HTTP_POST_VARS, 'avt_');
-		$avt_u->sync();
-		header("Location: admavatar.php?"._rsidl);
-		exit();
-	}
-	
-	if ( !empty($edit) && empty($prl) ) {
-		$avt_r = new fud_avatar;
-		$avt_r->get($edit);
-		$avt_r->export_vars('avt_');
-	}
-	
-	if ( !empty($btn_submit) ) {
-		$avt = new fud_avatar;
-		$avt->fetch_vars($HTTP_POST_VARS, 'avt_');
-		$avt->add();
-		header("Location: admavatar.php?"._rsidl);
-		exit();
-	}
-	
-	if ( !empty($icoul_size) ) {
-		/* check extention */
-		if ( preg_match('/.*\.(jpg|jpeg|gif|png)$/i', $icoul_name) ) {
-			$err = 0;
-			if ( !empty($ico_lfname) && !preg_match('/.*\.(jpg|jpeg|gif|png)$/i', $ico_lfname) ) {
-				$err = 1;
-				$err2 = 1;
+	fud_use('widgets.inc', true);
+
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
+
+	if (isset($_GET['del'])) {
+		if (($im = q_singleval('SELECT img FROM '.$tbl.'avatar WHERE id='.(int)$_GET['del']))) {
+			q('DELETE FROM '.$tbl.'avatar WHERE id='.(int)$_GET['del']);
+			if (db_affected()) {
+				q('UPDATE '.$tbl.'users SET avatar_loc=NULL, avatar=0, avatar_approved=\'NO\' WHERE avatar='.(int)$_GET['del']);
 			}
-			
-			if ( empty($err) ) {
-				$dst_name = $avatar_dir.(( isset($ico_lfname) ) ? $ico_lfname : $icoul_name);
-				umask(0177);
-				move_uploaded_file($icoul, $dst_name);
-				$avt_img = $icoul_name;
-			}
+			@unlink($GLOBALS['WWW_ROOT_DISK'] . 'images/avatars/'.$im);
 		}
-		else $err = 1;
+	}
+	if (isset($_GET['edit'])) {
+		list($avt_img, $avt_descr) = db_saq('SELECT img, descr FROM '.$tbl.'avatar WHERE id='.(int)$_GET['edit']);
+		$edit = (int)$_GET['edit'];
+	} else {
+		$edit = $avt_img = $avt_descr = '';
 	}
 
-	include('admpanel.php'); 
+	if (isset($_FILES['icoul']) && $_FILES['icoul']['size'] && preg_match('!\.(jpg|jpeg|gif|png)$!i', $_FILES['icoul']['name'])) {
+		move_uploaded_file($_FILES['icoul']['tmp_name'], $GLOBALS['WWW_ROOT_DISK'] . 'images/avatars/' . $_FILES['icoul']['name']);
+		if (empty($_POST['avt_img'])) {
+			$_POST['avt_img'] = $_FILES['icoul']['name'];
+		}
+	}
+	
+	if (isset($_POST['btn_update'], $_POST['edit']) && !empty($_POST['avt_img'])) {
+		$old_img = q_singleval('SELECT img FROM '.$tbl.'avatar WHERE id='.(int)$_POST['edit']);
+		q('UPDATE '.$tbl.'avatar SET img='.strnull(addslashes($_POST['avt_img'])).', descr='.strnull(addslashes($_POST['avt_descr'])).' WHERE id='.(int)$_POST['edit']);
+		if (db_affected() && $old_img != $_POST['avt_img']) {
+			$size = getimagesize($GLOBALS['WWW_ROOT_DISK'] . 'images/avatars/' . $_POST['avt_img']); 
+			$new_loc = '<img src="'.$GLOBALS['WWW_ROOT'].'images/avatars/'.$_POST['avt_img'].'" '.$size[3].' />';
+			q('UPDATE '.$tbl.'users SET avatar_loc=\''.$new_loc.'\' WHERE avatar='.(int)$_POST['edit']);
+		}
+	} else if (isset($_POST['btn_submit']) && !empty($_POST['avt_img'])) {
+		q('INSERT INTO '.$tbl.'avatar (img, descr) VALUES ('.strnull(addslashes($_POST['avt_img'])).', '.strnull(addslashes($_POST['avt_descr'])).')');
+	}
+
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>Avatar Management System</h2>
 
-<form name="frm_avt" method="post" enctype="multipart/form-data">
+<form name="frm_avt" method="post" action="admavatar.php" enctype="multipart/form-data">
 <?php echo _hs; ?>
 <table border=0 cellspacing=1 cellpadding=3>
-	<?php if ( is_writeable($avatar_dir) ) { ?>	
+	<?php if (@is_writeable($GLOBALS['WWW_ROOT_DISK'] . 'images/avatars')) { ?>
 		<tr bgcolor="#bff8ff">
 			<td colspan=2><b>Avatar Upload (upload avatars into the system)</td>
 		</tr>
 		<tr bgcolor="#bff8ff">
-			<td>Avatar Upload:</td>
-			<td>
-				<input type="file" name="icoul"> <input type="submit" name="btn_upload" value="Upload">
-				<?php if ( !empty($err) ) { ?>
-					<br><font size=-1 color="#ff0000">Only (*.gif, *.jpg, *.png) files are supported</font>
-				<?php } ?>
-			</td>
+			<td>Avatar Upload:<br><font size="-1">Only (*.gif, *.jpg, *.png) files are supported</font></td>
+			<td><input type="file" name="icoul"> <input type="submit" name="btn_upload" value="Upload"></td>
 		</tr>
 	<?php } else { ?>
 		<tr bgcolor="#bff8ff"> 
-			<td colspan=2><font color="#ff0000">Web server doesn't have write permission to write to <b>'<?php echo realpath($avatar_dir); ?>'</b>, avatar upload disabled</font></td>
+			<td colspan=2><font color="#ff0000">Web server doesn't have write permission to write to <b>'<?php echo $GLOBALS['WWW_ROOT_DISK'] . 'images/avatars'; ?>'</b>, avatar upload disabled</font></td>
 		</tr>
 	<?php } ?>
 	
@@ -118,28 +87,29 @@
 
 	<tr bgcolor="#bff8ff">
 		<td>Avatar Description:</td>
-		<td><input type="text" name="avt_descr" value="<?php echo (empty($avt_descr)?'':htmlspecialchars($avt_descr)); ?>"></td>
+		<td><input type="text" name="avt_descr" value="<?php echo htmlspecialchars($avt_descr); ?>"></td>
 	</tr>
 	
 	<tr bgcolor="#bff8ff">
 		<td valign=top><a name="avt_sel">Avatar Image:</a></td>
 		<td>
-			<input type="text" name="avt_img" value="<?php echo (empty($avt_img)?'':htmlspecialchars($avt_img)); ?>" 
+			<input type="text" name="avt_img" value="<?php echo htmlspecialchars($avt_img); ?>" 
 				onChange="javascript: 
-					if ( document.frm_avt.avt_img.value.length ) 
-						document.prev_icon.src='<?php echo $avatar_dir; ?>' + document.frm_avt.avt_img.value; 
-					else 
-						document.prev_icon.src='../blank.gif';">
-			[<a href="#avt_sel" onClick="javascript:window.open('admavatarsel.php?<?php echo _rsid; ?>', 'admavatarsel', 'menubar=false,scrollbars=yes,resizable=yes,height=300,width=500,screenX=100,screenY=100');">SELECT AVATAR</a>]
+					if (document.frm_avt.avt_img.value.length) {
+						document.prev_icon.src='<?php echo $GLOBALS['WWW_ROOT_DISK']; ?>images/avatars/' + document.frm_avt.avt_img.value; 
+					} else {
+						document.prev_icon.src='../blank.gif';
+					}">
+			[<a href="#avt_sel" onClick="javascript:window.open('admavatarsel.php?<?php echo _rsidl; ?>', 'admavatarsel', 'menubar=false,scrollbars=yes,resizable=yes,height=300,width=500,screenX=100,screenY=100');">SELECT AVATAR</a>]
 		</td>
 	</tr>
-	
+
 	<tr bgcolor="#bff8ff">
 		<td>Preview Image:</td>
 		<td>
 			<table border=1 cellspacing=1 cellpadding=2 bgcolor="#ffffff">
 				<tr><td align=center valign=center>
-					<img src="<?php echo ( strlen($avt_img) )?$avatar_dir.$avt_img:'../blank.gif'; ?>" name="prev_icon" border=0>
+					<img src="<?php echo ($avt_img ? $GLOBALS['WWW_ROOT'] . 'images/avatars/' . $avt_img : '../blank.gif'); ?>" name="prev_icon" border=0>
 				</td></tr>
 			</table>
 		</td>
@@ -147,20 +117,17 @@
 	
 	<tr bgcolor="#bff8ff">
 		<?php
-			if ( empty($edit) ) {
+			if (!$edit) {
 				echo '<td colspan=2 align=right><input type="submit" name="btn_submit" value="Add Avatar"></td>';
-			}
-			else {
-				echo '<td colspan=2 align=right><input type="submit" name="btn_cancel" value="Cancel">
-				      <input type="submit" name="btn_update" value="Update"></td>';
+			} else {
+				echo '<td colspan=2 align=right><input type="submit" name="btn_cancel" value="Cancel"><input type="submit" name="btn_update" value="Update"></td>';
 			}
 		?>
 	</tr>
-	
 </table>
-<input type="hidden" name="edit" value="<?php echo (empty($edit)?'':$edit); ?>">
-<input type="hidden" name="prl" value="1">
+<input type="hidden" name="edit" value="<?php echo $edit; ?>">
 </form>
+
 <table border=0 cellspacing=3 cellpadding=2>
 <tr bgcolor="#e5ffe7">
 	<td>Avatar</td>
@@ -168,18 +135,21 @@
 	<td align="center">Action</td>
 </tr>
 <?php
-	$avt_draw = new fud_avatar;
-	$avt_draw->getall();
-	$avt_draw->resets();
-	
-	$i=0;
-	while ( $obj = $avt_draw->eachs() ) {
-		$bgcolor = ($i++%2)?' bgcolor="#fffee5"':'';
-		if ( !empty($edit) && $edit==$obj->id ) $bgcolor =' bgcolor="#ffb5b5"';
-		
-		$ctl = "<td>[<a href=\"admavatar.php?edit=".$obj->id."&"._rsid."#img\">Edit</a>] [<a href=\"admavatar.php?del=".$obj->id."&"._rsid."\">Delete</a>]</td>";
-		echo "<tr".$bgcolor."><td><img src=\"".$avatar_dir.$obj->img."\" border=0></td><td>".$obj->descr."</td>".$ctl."</tr>\n";
-	}  
+	$c = uq('SELECT id, img, descr FROM '.$tbl.'avatar');
+	$i = 0;
+	while ($r = db_rowarr($c)) {
+		if ($edit == $r[0]) {
+			$bgcolor = ' bgcolor="#ffb5b5"';
+		} else {
+			$bgcolor = ($i++%2) ? ' bgcolor="#fffee5"' : '';
+		}
+		echo '<tr '.$bgcolor.'>
+				<td><img src="'.$GLOBALS['WWW_ROOT'].'images/avatars/'.$r[1].'" alt="'.$r[2].'" border=0 /></td>
+				<td>'.$r[2].'</td>
+				<td>[<a href="admavatar.php?edit='.$r[0].'&'._rsidl.'#img">Edit</a>] [<a href="admavatar.php?del='.$r[0].'&'._rsidl.'">Delete</a>]</td>
+			</tr>';
+	}
+	qf($c);
 ?>
 </table>
-<?php readfile('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
--- install/www_root/adm/admavatarsel.php
+++ install/www_root/adm/admavatarsel.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admavatarsel.php,v 1.1.1.1 2002/06/17 23:00:09 hackie Exp $
+*   $Id: admavatarsel.php,v 1.2 2003/04/28 14:12:23 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -16,43 +16,35 @@
 ***************************************************************************/
 
 	define('admin_form', 1);
+	require('GLOBALS.php');
+	fud_use('adm.inc', true);
 
-	include_once "GLOBALS.php";
-	
-	fud_use('util.inc');
-
-	cache_buster();
-	
-	$avatar_dir = '../images/avatars/';
-	
 	echo '<html><body bgcolor="#ffffff">';
 	
 	/* here we draw the avatar control */
-			if ( !is_readable($avatar_dir) ) {
-				echo "<br><font color=\"#ff0000\">Can't read '$avatar_dir'</font></br>";
+	if ($dp = opendir($GLOBALS['WWW_ROOT_DISK'] . 'images/avatars')) {
+		readdir($dp); readdir($dp);
+		$icons_per_row = 7;
+		$col = $i = 0;
+		echo '<table border=0 cellspacing=1 cellpadding=2><tr>';
+		while ($de = readdir($dp)) {
+			$ext = strtolower(substr($de, -4));
+			if ($ext != '.gif' && $ext != '.jpg' && $ext != '.png' && $ext != 'jpeg') {
+				continue;
 			}
-			else {
-				$icons_per_row = 7;
-				if ( $dp = opendir($avatar_dir) ) {
-					echo "<table border=0 cellspacing=1 cellpadding=2><tr>";
-					$col = $i = 0;
-					while ( $de = readdir($dp) ) {
-			 			if ( $de == '.' || $de == '..' ) continue;
-						if ( strlen($de) < 4 ) continue;
-						$ext = strtolower(substr($de, -4));
-						if ( $ext != '.gif' && $ext != '.jpg' && $ext != '.png' ) continue;
-				
-						if ( !($col++%$icons_per_row) ) {
-							echo "\n</tr>\n<tr>\n";
-						}
-	
-						$bgcolor = ( !($i++%2) ) ? ' bgcolor="#f4f4f4"':'';
-				
-						echo "<td$bgcolor nowrap valign=center align=center><a href=\"javascript: window.opener.document.prev_icon.src='$avatar_dir$de'; window.opener.document.frm_avt.avt_img.value='$de'; window.close();\"><img src=\"$avatar_dir$de\" border=0><br><font size=-2>$de</font></a></td>";
-			 		}
-			 		closedir($dp);
-				 	echo '</tr></table>';
-				}
+			if (!($col++%$icons_per_row)) {
+				echo '</tr><tr>';
 			}
-	echo '</body></html>'; 
+			$bgcolor = (!($i++%2)) ? ' bgcolor="#f4f4f4"' : '';
+
+			echo '<td '.$bgcolor.' nowrap valign=center align=center><a href="javascript: window.opener.document.prev_icon.src=\'../images/avatars/'.$de.'\'; window.opener.document.frm_avt.avt_img.value=\''.$de.'\'; window.close();"><img src="../images/avatars/'.$de.'" border=0><br><font size=-2>'.$de.'</font></a></td>';
+		}
+		closedir($dp);
+		echo '</tr></table>';
+	}
+	if (!$i) {
+		echo 'There are no built-in avatars';	
+	}
+
+	echo '</body></html>';
 ?>
