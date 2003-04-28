0834c2491e8d2d211771c32f6a4a2c513c074573
iliaa <ilia@ilia.ws>
2003-04-28 16:12:04 +0000

    Mime stuff.

--- install/www_root/adm/admmime.php
+++ install/www_root/adm/admmime.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admmime.php,v 1.6 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admmime.php,v 1.7 2003/04/28 16:12:04 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,81 +17,55 @@
 
 	define('admin_form', 1);
 	
-	include_once "GLOBALS.php";
-	
-	fud_use('widgets.inc', true);
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('mime_adm.inc', true);
-	fud_use('util.inc');
-	fud_use('objutil.inc');
-	
-	list($ses, $usr) = initadm();
-	
-	$web_path = '../images/mime/';
-	
-	if( !empty($HTTP_POST_VARS['prl']) ) {
-		if( empty($HTTP_POST_FILES['icoul']['size']) ) {
-			$mime = new fud_mime;
-			fetch_vars('mime_', $mime, $HTTP_POST_VARS);
-		
-			if( empty($edit) )
-				$mime->add();
-			else
-				$mime->sync($edit);
-		
-			header("Location: admmime.php?edit=".$mime->id."&"._rsidl);
-			exit;
-		}
-		else {	
-			if ( preg_match('!\..*(jpg|jpeg|gif|png)$!i', $HTTP_POST_FILES['icoul']['name']) ) {
-				$err = 0;
-				umask(0177);
-				move_uploaded_file($HTTP_POST_FILES['userfile']['tmp_name'], '../images/mime/'.$HTTP_POST_FILES['icoul']['name']);
-				$mime_icon = $HTTP_POST_FILES['icoul']['name'];
-				$prev_icon = $web_path.$HTTP_POST_FILES['icoul']['name'];
-			}
-			else 
-				$err = 1;
-		}	
-	}
-	else if( !empty($edit) ) {
-		$mime = new fud_mime;
-		$mime->get($edit);
-		export_vars('mime_', $mime);
-	}
-	else if( $del ) {
-		$mime = new fud_mime;
-		$mime->get($del);
-		$mime->delete();
-		
-		header("Location: admmime.php?"._rsidl);
-		exit;		
+	fud_use('widgets.inc', true);
+
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
+
+	if (isset($_GET['del'])) {
+		q('DELETE FROM '.$tbl.'mime WHERE id='.(int)$_GET['del']);
 	}
 
-	include('admpanel.php'); 
+	if (isset($_GET['edit'])) {
+		list($mime_descr, $mime_mime_hdr, $mime_fl_ext, $mime_icon) = db_saq('SELECT descr, mime_hdr, fl_ext, icon FROM '.$tbl.'mime WHERE id='.(int)$_GET['edit']);
+		$edit = (int)$_GET['edit'];
+	} else {
+		$mime_icon = $edit = $mime_descr = $mime_mime_hdr = $mime_fl_ext = '';
+	}
+	
+	if (isset($_FILES['icoul']) && $_FILES['icoul']['size'] && preg_match('!\.(jpg|jpeg|gif|png)$!i', $_FILES['icoul']['name'])) {
+		move_uploaded_file($_FILES['icoul']['tmp_name'], $GLOBALS['WWW_ROOT_DISK'] . 'images/mime/' . $_FILES['icoul']['name']);
+		if (empty($_POST['mime_icon'])) {
+			$_POST['mime_icon'] = $_FILES['icoul']['name'];
+		}
+	}
+
+	if (isset($_POST['btn_update'], $_POST['edit'])) {
+		q('UPDATE '.$tbl.'mime SET descr='.strnull(addslashes($_POST['mime_descr'])).', mime_hdr='.strnull(addslashes($_POST['mime_mime_hdr'])).', fl_ext='.strnull(addslashes($_POST['mime_fl_ext'])).', icon='.strnull(addslashes($_POST['mime_icon'])).' WHERE id='.(int)$_POST['edit']);
+	} else if (isset($_POST['btn_submit'])) {
+		q('INSERT INTO '.$tbl.'mime (descr, mime_hdr, fl_ext, icon) VALUES ('.strnull(addslashes($_POST['mime_descr'])).', '.strnull(addslashes($_POST['mime_mime_hdr'])).', '.strnull(addslashes($_POST['mime_fl_ext'])).', '.strnull(addslashes($_POST['mime_icon'])).')');
+	}
+
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>MIME Management System</h2>
 <table border=0 cellspacing=1 cellpadding=3>
 <form action="admmime.php" name="frm_mime" method="post" enctype="multipart/form-data">
 <?php 
 	echo _hs; 
-	if ( @is_writeable(realpath('../images/mime/')) ) {
+	if (@is_writeable($GLOBALS['WWW_ROOT_DISK'] . 'images/mime/')) {
 ?>	
 <tr bgcolor="#bff8ff">
 	<td colspan=2><b>MIME Icon Upload (upload mime icons into the system)</td>
 </tr>
 <tr bgcolor="#bff8ff">
-	<td>MIME Icon Upload:</td>
-	<td>
-		<input type="file" name="icoul"> <input type="submit" name="btn_upload" value="Upload">
-		<?php if ( !empty($err) ) { ?>
-			<br><font size=-1 color="#ff0000">Only (.gif, *.jpg, *.png) files are supported</font>
-		<?php } ?>
-	</td>
+	<td>MIME Icon Upload:<br><font size="-1">Only (.gif, *.jpg, *.png) files are supported</font></td>
+	<td><input type="file" name="icoul"> <input type="submit" name="btn_upload" value="Upload"></td>
 </tr>
 <?php } else { ?>
 <tr bgcolor="#bff8ff"> 
-	<td colspan=2><font color="#ff0000">Web server does not have write permissions to <b>'<?php echo realpath('../images/mime/'); ?>'</b>, mime icon upload disabled</font></td>
+	<td colspan=2><font color="#ff0000">Web server does not have write permissions to <b>'<?php echo $GLOBALS['WWW_ROOT_DISK']; ?>images/mime/'</b>, mime icon upload disabled</font></td>
 </tr>
 <?php } ?>
 <tr><td colspan=2>&nbsp;</td></tr>
@@ -117,28 +91,35 @@
 		
 <tr bgcolor="#bff8ff">
 	<td valign="top"><a name="mime_sel">MIME Icon:</a></td>
-	<td><input type="text" name="mime_icon" value="<?php echo htmlspecialchars($mime_icon); ?>" onChange="javascript: if( document.frm_sml.mime_icon.value.length ) document.prev_icon.src='<?php echo $web_path; ?>' + document.frm_sml.mime_icon.value; else document.prev_icon.src='<?php echo $web_path; ?>blank.gif';"> [<a href="#mime_sel" onClick="javascript:window.open('admmimesel.php?<?php echo _rsid; ?>', 'admmimesel', 'menubar=false,scrollbars=yes,resizable=yes,height=300,width=500,screenX=100,screenY=100');">select MIME icon</a>]</td>
+	<td nowrap><input type="text" name="mime_icon" value="<?php echo htmlspecialchars($mime_icon); ?>" onChange="javascript: 
+				if (document.frm_sml.mime_icon.value.length) {
+					document.prev_icon.src='<?php echo $GLOBALS['WWW_ROOT']; ?>images/mime/' + document.frm_sml.mime_icon.value;
+				} else {
+					document.prev_icon.src='../blank.gif';
+				}"> [<a href="#mime_sel" onClick="javascript:window.open('admmimesel.php?<?php echo _rsidl; ?>', 'admmimesel', 'menubar=false,scrollbars=yes,resizable=yes,height=300,width=500,screenX=100,screenY=100');">select MIME icon</a>]</td>
 </tr>
 	
 <tr bgcolor="#bff8ff">
 	<td valign="top">Preview Image:</td>
 	<td>
 		<table border=1 cellspacing=1 cellpadding=2 bgcolor="#ffffff">
-		<tr><td align=center valign=center><img src="<?php echo $web_path.(!empty($mime_icon)?$mime_icon:'unknown.gif'); ?>" name="prev_icon" border=0></td></tr>
+		<tr><td align=center valign=center><img src="<?php echo $GLOBALS['WWW_ROOT'] . 'images/mime/' . $mime_icon; ?>" name="prev_icon" border=0></td></tr>
 		</table>
 	</td>
 </tr>
 	
 <tr bgcolor="#bff8ff">
+	<td colspan=2 align=right><input type="submit" name="btn_cancel" value="Reset"> 
 <?php
-	if ( empty($edit) ) 
-		echo '<td colspan=2 align=right><input type="reset" name="btn_cancel" value="Reset"> <input type="submit" name="btn_submit" value="Add MIME"></td>';
-	else 
-		echo '<td colspan=2 align=right><input type="reset" name="btn_cancel" value="Reset"> <input type="submit" name="btn_update" value="Update"></td>';
+	if (!$edit) {
+		echo '<input type="submit" name="btn_submit" value="Add MIME">';
+	} else {
+		echo '<input type="submit" name="btn_update" value="Update"></td>';
+	}
 ?>
+	</td>
 </tr>
 <input type="hidden" name="edit" value="<?php echo $edit; ?>">
-<input type="hidden" name="prl" value="1">
 </form>
 </table>
 <p>
@@ -151,13 +132,17 @@
 	<td align="center">Action</td>
 </tr>
 <?php
-	$r = q("SELECT * FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."mime");
-	$i=1;
-	while( $obj = db_rowobj($r) ) {
-		$bgcolor = ($i++%2)?' bgcolor="#fffee5"':'';
-		if ( !empty($edit) && $edit==$obj->id ) $bgcolor =' bgcolor="#ffb5b5"';
-		echo '<tr'.$bgcolor.' valign="top"><td><img src="'.$web_path.$obj->icon.'" border=0></td><td>'.$obj->mime_hdr.'</td><td>'.$obj->descr.'</td><td>'.$obj->fl_ext.'</td><td nowrap>[<a href="admmime.php?edit='.$obj->id.'&'._rsid.'#img">Edit</a>] [<a href="admmime.php?del='.$obj->id.'&'._rsid.'">Delete</a>]</td></tr>';
+	$c = uq('SELECT id, icon, mime_hdr, fl_ext, descr FROM '.$tbl.'mime');
+	$i = 1;
+	while ($r = db_rowarr($c)) {
+		if ($edit == $r[0]) {
+			$bgcolor = ' bgcolor="#ffb5b5"';
+		} else {
+			$bgcolor = ($i++%2) ? ' bgcolor="#fffee5"' : '';
+		}
+		echo '<tr'.$bgcolor.' valign="top"><td><img src="'.$GLOBALS['WWW_ROOT'].'images/mime/'.$r[1].'" border=0></td><td>'.$r[2].'</td><td>'.$r[4].'</td><td>'.$r[3].'</td><td nowrap>[<a href="admmime.php?edit='.$r[0].'&'._rsidl.'#img">Edit</a>] [<a href="admmime.php?del='.$r[0].'&'._rsidl.'">Delete</a>]</td></tr>';
 	}
+	qf($c);
 ?>
 </table>
-<?php require('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
--- install/www_root/adm/admmimesel.php
+++ install/www_root/adm/admmimesel.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admmimesel.php,v 1.1.1.1 2002/06/17 23:00:09 hackie Exp $
+*   $Id: admmimesel.php,v 1.2 2003/04/28 16:12:04 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -16,39 +16,34 @@
 ***************************************************************************/
 
 	define('admin_form', 1);
-	
-	include_once "GLOBALS.php";
-	
-	fud_use('util.inc');
+	require('GLOBALS.php');
+	fud_use('adm.inc', true);
 
-	cache_buster();
-	
-	$mime_path = '../images/mime/';
-	
 	echo '<html><body bgcolor="#ffffff">';
-	
-	if ( !@is_readable($mime_path) ) 
-		echo '<br><font color="#ff0000">Can\'t read "'.$mime_path.'"</font></br>';
-	else {
-		$icons_per_row = 7;
-		$olddir = getcwd();
-		chdir($mime_path);
-		$dp = opendir('.');
+
+	if ($dp = opendir($GLOBALS['WWW_ROOT_DISK'] . 'images/mime')) {
 		readdir($dp); readdir($dp);
-		
-		echo '<table border=0 cellspacing=1 cellpadding=2><tr>';
+		$icons_per_row = 7;
 		$col = $i = 0;
-		while ( $de = readdir($dp) ) {
-			if( is_dir($de) ) continue;
+		echo '<table border=0 cellspacing=1 cellpadding=2><tr>';
+		while ($de = readdir($dp)) {
 			$ext = strtolower(substr($de, -4));
-			if ( $ext != '.gif' && $ext != 'jpeg' && $ext != '.jpg' && $ext != '.png' ) continue;
-			if ( !($col++%$icons_per_row) ) echo '</tr><tr>';
-			$bgcolor = ( !($i++%2) ) ? ' bgcolor="#f4f4f4"':'';
-				
-			echo '<td'.$bgcolor.' nowrap valign=center align=center><a href="javascript: window.opener.document.prev_icon.src=\''.$mime_path.$de.'\'; window.opener.document.frm_mime.mime_icon.value=\''.$de.'\'; window.close();"><img src="'.$mime_path.$de.'" border=0><br><font size=-2>'.$de.'</font></a></td>';
+			if ($ext != '.gif' && $ext != '.jpg' && $ext != '.png' && $ext != 'jpeg') {
+				continue;
+			}
+			if (!($col++%$icons_per_row)) {
+				echo '</tr><tr>';
+			}
+			$bgcolor = (!($i++%2)) ? ' bgcolor="#f4f4f4"' : '';
+
+			echo '<td '.$bgcolor.' nowrap valign=center align=center><a href="javascript: window.opener.document.prev_icon.src=\'../images/mime/'.$de.'\'; window.opener.document.frm_mime.mime_icon.value=\''.$de.'\'; window.close();"><img src="../images/mime/'.$de.'" border=0><br><font size=-2>'.$de.'</font></a></td>';
 		}
 		closedir($dp);
 		echo '</tr></table>';
 	}
-	echo '</body></html>'; 
+	if (!$i) {
+		echo 'There are no mime icons';	
+	}
+
+	echo '</body></html>';
 ?>
