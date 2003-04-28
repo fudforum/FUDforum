83602aa73dd3509fdb356a867d80aa048e7df4a6
iliaa <ilia@ilia.ws>
2003-04-28 12:58:08 +0000

    Fixes.

--- install/www_root/adm/admapprove_avatar.php
+++ install/www_root/adm/admapprove_avatar.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admapprove_avatar.php,v 1.8 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admapprove_avatar.php,v 1.9 2003/04/28 12:58:08 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -18,96 +18,36 @@
 	define('admin_form', 1);
 	define('no_inline', 1);
 
-	include_once "GLOBALS.php";
-	
-	fud_use('db.inc');
-	fud_use('time.inc');
-	fud_use('cookies.inc');
-	fud_use('users.inc');
-	fud_use('ssu.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('util.inc');
-	fud_use('util_adm.inc', true);
-	
-	list($ses, $usr) = initadm();
-	
-	$avatar_dir = '../images/custom_avatars/';
-	
-	/* check for convert */
-	if ( !empty($fixit) && $MOGRIFY_BIN ) {
-		exec($MOGRIFY_BIN.' -geometry '.$GLOBALS['CUSTOM_AVATAR_MAX_DIM'].' +profile iptc +profile icm +comment '.realpath($avatar_dir).$fixit);
-		header("Location: admapprove_avatar.php?"._rsidl."&rand=".get_random_value());
-		exit();
-	}
-	
-	if ( !empty($usr_id) ) {
-		$usr_a = new fud_user_adm;
-		$usr_a->get_user_by_id($usr_id);
-		$usr_a->approve_avatar();
-		header("Location: admapprove_avatar.php?"._rsidl."&rand=".get_random_value());
-		exit();
-	}
-	
-	if ( !empty($del) ) {
-		$usr_a = new fud_user_adm;
-		$usr_a->get_user_by_id($del);
-		if ( !strlen($usr_a->avatar_loc) ) 
-			@unlink($avatar_dir.$del);
+	fud_use('ssu.inc');
 
-		$usr_a->unapprove_avatar();
-		header("Location: admapprove_avatar.php?"._rsidl."&rand=".get_random_value());
-		exit();
+	if (isset($_GET['usr_id'])) {
+		usr_approve_avatar((int)$_GET['usr_id']);
+	} else if (isset($_GET['del'])) {
+		usr_unapprove_avatar((int)$_GET['del']);
 	}
-	cache_buster();
-	include('admpanel.php'); 
+
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>Avatar Approval System</h2>	
 <table border=0 cellspacing=0 cellpadding=3>
 <?php
-	$a=0;
+	$a = 0;
 	
-	$r = q("SELECT * FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE avatar_loc IS NULL AND avatar_approved='N' ORDER BY id");
-	while ( $obj = db_rowobj($r) ) {
-		if ( $MOGRIFY_BIN ) {
-			$fix_it = ' [<a href="admapprove_avatar.php?fixit='.$obj->id.'&'._rsid.'">Fix It</a>]';
-		}
-		
-		$a=1;
-		
-		echo '<tr bgcolor="#bff8ff"><td>
-		<table border=0 cellspacing=0 cellpadding=0 width="100%">
-		<tr><td align=left>
-		'.$obj->alias.'
-		</td>
-		<td align=right>
-			[<a href="admapprove_avatar.php?usr_id='.$obj->id.'&'._rsid.'">Approve</a>] [<a href="admapprove_avatar.php?del='.$obj->id.'&'._rsid.'">Delete</a>]'.$fix_it.'
-		</td>
-		</table>
-		
-		</td></tr>';
-		echo '<tr bgcolor="#bff8ff"><td align=center>(local)<br><img src="'.$avatar_dir.$obj->id.'?rnd='.get_random_value().'" border=0></td></tr>';
-		
+	$c = uq('SELECT id, avatar_loc, alias FROM '.$GLOBALS['DBHOST_TBL_PREFIX'].'users WHERE avatar_approved=\'N\' ORDER BY id');
+	while ($r = db_rowarr($c)) {
+		$a = 1;
+		echo '<tr bgcolor="#bff8ff"><td>'.$r[2].'</td><td>[<a href="admapprove_avatar.php?usr_id='.$r[0].'&'._rsidl.'">Approve</a>] [<a href="admapprove_avatar.php?del='.$r[0].'&'._rsidl.'">Delete</a>]</td></tr>';
+		echo '<tr bgcolor="#bff8ff"><td align="center" colspan=2>'.$r[1].'</td></tr>';
 	}
-	qf($r);
-
-	$r = q("SELECT * FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE avatar_loc IS NOT NULL AND avatar_approved='N' ORDER BY id");
-	while ( $obj = db_rowobj($r) ) {
-		$a=1;
-		echo '<tr bgcolor="#bff8ff"><td>
-		<table border=0 cellspacing=0 cellpadding=0 width="100%">
-		<tr><td align=left>
-		'.$obj->alias.'
-		</td>
-		<td align=right>
-			[<a href="admapprove_avatar.php?usr_id='.$obj->id.'&'._rsid.'">Approve</a>] [<a href="admapprove_avatar.php?del='.$obj->id.'&'._rsid.'">Delete</a>]
-		</td>
-		</table>
-		
-		</td></tr>';
-		echo '<tr bgcolor="#bff8ff"><td align=middle>'.$obj->avatar_loc.'<br><img src="'.$obj->avatar_loc.'" border=0></td></tr>';
-	}
-	qf($r);
+	qf($c);
 ?>
 </table>
-<?php if( empty($a) ) echo 'There is nothing to approve.'; ?>
-<?php readfile('admclose.html'); ?>
+<?php
+	if (!$a) {
+		echo 'There are no avatars pending approval.';
+	}
+
+	require($WWW_ROOT_DISK . 'adm/admclose.html');
+?>
