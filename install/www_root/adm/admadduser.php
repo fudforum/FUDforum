a37456d80bebf5cb978ee0eb212402ea80c06fc5
iliaa <ilia@ilia.ws>
2003-04-29 15:48:03 +0000

    Cleanup

--- install/www_root/adm/admadduser.php
+++ install/www_root/adm/admadduser.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admadduser.php,v 1.1 2002/11/11 04:30:50 hackie Exp $
+*   $Id: admadduser.php,v 1.2 2003/04/29 15:48:03 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,16 +17,9 @@
 
 	define('admin_form', 1);
 	
-	include_once "GLOBALS.php";
-	
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('users.inc');	
 	fud_use('widgets.inc', true);
-	fud_use('util.inc');
-	
-	list($ses, $usr_adm) = initadm();
-	
-	cache_buster();
 
 	$error = NULL;
 
@@ -35,60 +28,53 @@ function errorify($err)
 	return '<font color="red">'.$err.'</font><br>';
 }
 
-function validate_input($input)
+function validate_input()
 {
-	if (empty($input['login'])) {
-		$GLOBALS['err_login'] = errorify("Login cannot be blank");
+	if (empty($_POST['login'])) {
+		$GLOBALS['err_login'] = errorify('Login cannot be blank');
 		return 1;
 	}
 	
-	if (bq("SELECT id FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE login='".$input['login']."'")) {
-		$GLOBALS['err_login'] = errorify("Login (".stripslashes($input['login']).") is already in use.");
+	if (q_singleval('SELECT id FROM '.$GLOBALS['DBHOST_TBL_PREFIX'].'users WHERE login=\''.addslashes($_POST['login']).'\'')) {
+		$GLOBALS['err_login'] = errorify('Login ('.htmlspecialchars($_POST['login']).') is already in use.');
 		return 1;
 	}
 	
-	if (empty($input['passwd'])) {
-		$GLOBALS['err_passwd'] = errorify("Password cannot be blank");
+	if (empty($_POST['passwd'])) {
+		$GLOBALS['err_passwd'] = errorify('Password cannot be blank');
 		return 1;
 	}
 	
-	if (empty($input['email'])) {
-		$GLOBALS['err_email'] = errorify("E-mail cannot be blank");
+	if (empty($_POST['email'])) {
+		$GLOBALS['err_email'] = errorify('E-mail cannot be blank');
 		return 1;
 	}	
 	
-	if (bq("SELECT id FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE email='".$input['email']."'")) {
-		$GLOBALS['err_email'] = errorify("Email (".stripslashes($input['email']).") is already in use.");
+	if (q_singleval('SELECT id FROM '.$GLOBALS['DBHOST_TBL_PREFIX'].'users WHERE email=\''.addslashes($_POST['email']).'\'')) {
+		$GLOBALS['err_email'] = errorify('Email ('.htmlspecialchars($_POST['email']).') is already in use.');
 		return 1;
 	}
 	
 	return 0;
 }
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
 
-	if (count($HTTP_POST_VARS) && !($error=validate_input($HTTP_POST_VARS))) {
-	
-		$default_theme = q_singleval("SELECT id FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."themes WHERE t_default='Y' AND enabled='Y'");
-	
-		db_lock($GLOBALS['DBHOST_TBL_PREFIX'].'users+');
+	if (isset($_POST['usr_add']) && !($error = validate_input())) {
+		$default_theme = q_singleval('SELECT id FROM '.$tbl.'themes WHERE t_default=\'Y\' AND enabled=\'Y\'');
+		$alias = addslashes(htmlspecialchars($_POST['login']));
+		db_lock($tbl.'users WRITE');
 	
 		if ($GLOBALS['USE_ALIASES'] == 'Y') {
-			$alias = addslashes(htmlspecialchars(stripslashes($HTTP_POST_VARS['login'])));
-			$alias_len = strlen($alias);
 			$i = 0;
-			while (bq("SELECT id FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users WHERE alias='".$alias."'")) {
-				if ($i) {
-					$alias = substr_replace($alias, ' '.$i, $alias_len, (strlen($i-1) + 1));
-					$i++;
-				} else {
-					$alias .= ' 1';
-					$i++;
-				}	
+			while (q_singleval('SELECT id FROM '.$tbl.'users WHERE alias=\''.(!$i ? $alias : $alias . '_' . $i).'\'')) {
+				++$i;
 			}
-		} else {
-			$alias = addslashes(htmlspecialchars(stripslashes($HTTP_POST_VARS['login'])));
+			if ($i) {
+				$alias .= '_' . $i;
+			}
 		}
 		
-		$r = q("INSERT INTO ".$GLOBALS['DBHOST_TBL_PREFIX']."users (
+		$user_added = db_qid("INSERT INTO ".$tbl."users (
 			login,
 			alias,
 			passwd,
@@ -103,11 +89,11 @@ function validate_input($input)
 			email_conf		
 			)
 			VALUES(
-			'".$HTTP_POST_VARS['login']."',
+			'".addslashes($_POST['login'])."',
 			'".$alias."',
-			'".md5($HTTP_POST_VARS['passwd'])."',
-			'".$HTTP_POST_VARS['name']."',
-			'".$HTTP_POST_VARS['email']."',
+			'".md5($_POST['passwd'])."',
+			'".addslashes($_POST['name'])."',
+			'".addslashes($_POST['email'])."',
 			'".$GLOBALS['SERVER_TZ']."',
 			".__request_timestamp__.",
 			".$default_theme.",
@@ -117,24 +103,23 @@ function validate_input($input)
 			'Y'
 			)");
 			
-		if ($r) {
-			$user_added = 1;
-		}	
-			
-		db_unlock();	
+		db_unlock();
+		$login = $passwd = $email = $name = '';
+	} else {
+		$login = isset($_POST['login']) ? htmlspecialchars($_POST['login']) : '';
+		$passwd = isset($_POST['passwd']) ? htmlspecialchars($_POST['passwd']) : '';
+		$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+		$name = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : '';
 	}
 
-	require('admpanel.php'); 
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>Add User</h2>
 <?php
 	if (!empty($error)) {
 		echo '<h1 color="red">Error Has Occured</h1>';
-	}
-	
-	if (isset($user_added)) {
-		echo '<font size="+1" color="green">User ('.stripslashes($HTTP_POST_VARS['login']).') was successfully added.<font><br>';
-		$HTTP_POST_VARS = array();
+	} else if (!empty($user_added)) {
+		echo '<font size="+1" color="green">User ('.$_POST['login'].') was successfully added.<font><br>';
 	}
 ?>
 <form name="frm_usr" method="post" action="admadduser.php">
@@ -146,23 +131,23 @@ function validate_input($input)
 	
 	<tr bgcolor="#f1f1f1">
 		<td>Login:</td>
-		<td><?php echo $err_login; ?><input type="text" name="login" value="<?php echo stripslashes($HTTP_POST_VARS['login']); ?>" size="30"></td>
+		<td><?php if (isset($err_login)) { echo $err_login; } ?><input type="text" name="login" value="<?php echo $login; ?>" size="30"></td>
 	</tr>
 	<tr bgcolor="#f1f1f1">
 		<td>Password:</td>
-		<td><?php echo $err_passwd; ?><input type="text" name="passwd" value="<?php echo stripslashes($HTTP_POST_VARS['passwd']); ?>" size="30"></td>
+		<td><?php if (isset($err_passwd)) { echo $err_passwd; } ?><input type="text" name="passwd" value="<?php echo $passwd; ?>" size="30"></td>
 	</tr>
 	<tr bgcolor="#f1f1f1">
 		<td>E-mail:</td>
-		<td><?php echo $err_email; ?><input type="text" name="email" value="<?php echo stripslashes($HTTP_POST_VARS['email']); ?>" size="30"></td>
+		<td><?php if (isset($err_email)) { echo $err_email; } ?><input type="text" name="email" value="<?php echo $email; ?>" size="30"></td>
 	</tr>
 	<tr bgcolor="#f1f1f1">
 		<td>Real Name:</td>
-		<td><input type="text" name="name" value="<?php echo stripslashes($HTTP_POST_VARS['name']); ?>" size="30"></td>
+		<td><input type="text" name="name" value="<?php echo $name; ?>" size="30"></td>
 	</tr>
 	<tr bgcolor="#bff8ff">
 		<td colspan=2 align=right><input type="submit" value="Add User" name="usr_add"></td>
 	</tr>
 </table>
 </form>
-<?php require('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
