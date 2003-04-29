68af4132dddf071467c98798f05b57420f0ba467
iliaa <ilia@ilia.ws>
2003-04-29 15:10:51 +0000

    Fixes.

--- install/www_root/adm/admmassemail.php
+++ install/www_root/adm/admmassemail.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admmassemail.php,v 1.9 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admmassemail.php,v 1.10 2003/04/29 15:10:51 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,75 +17,62 @@
 	
 	define('admin_form', 1);
 	
-	include_once "GLOBALS.php";
-	
-	fud_use('db.inc');
-	fud_use('widgets.inc', true);
-	fud_use('util.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('users.inc');
+	fud_use('widgets.inc', true);
 	fud_use('smtp.inc');
 	
-	list($ses, $usr) = initadm();
-	
-	if ( $btn_submit ) {
-		if ( !$ignore_override ) $ignore_override_q = " WHERE ignore_admin='N'";
-		$r = q("SELECT email,name FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."users".$ignore_override_q);
+	if (isset($_POST['btn_submit']) && !empty($_POST['subject']) && !empty($_POST['body'])) {
+		$c = uq('SELECT email FROM '.$GLOBALS['DBHOST_TBL_PREFIX'].'users '.(isset($POST['ignore_override']) ? '' : 'WHERE ignore_admin=\'N\''));
 		$email_block = 50;
 		$email_block_stat = 0;
 		$send_to = $GLOBALS['ADMIN_EMAIL'];
+		$to = array();
 		
-		$subject = stripslashes($subject);
-		$body = stripslashes($body);
-		
-		if( $GLOBALS['USE_SMTP'] == 'N' ) {
-			while ( $obj = db_rowobj($r) ) {
-				$bcc .= $obj->email.', ';
-				if ( !(++$email_block_stat%$email_block) ) {
-					$email_block_stat=0;
-					$bcc = substr($bcc, 0, -2)."\r\n";
-					mail($send_to, $subject, $body, "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
-					$bcc = NULL;
+		if ($GLOBALS['USE_SMTP'] == 'N') {
+			while ($r = db_rowarr($c)) {
+				$to[] = $r[0];
+				if (!(++$email_block_stat % $email_block)) {
+					$email_block_stat = 0;
+					$send_to = array_pop($to);
+					$bcc = implode(', ', $to) . "\r\n";
+					mail($send_to, $_POST['subject'], $_POST['body'], "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
+					$to = array();
 				}
 			}
-		
-			if( $bcc ) {
-				$bcc = substr($bcc, 0, -2)."\r\n";
-				mail($send_to, $subject, $body, "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum\r\nBcc: ".$bcc);
+			if (count($to)) {
+				$send_to = array_pop($to);
+				$bcc = implode(', ', $to) . "\r\n";
+				mail($send_to, $_POST['subject'], $_POST['body'], "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
 			}
-		}
-		else {
-			$to = array();
+		} else {
 			$smtp = new fud_smtp;
-			$smtp->msg = $body;
-			$smtp->subject = $subject;
+			$smtp->msg = $_POST['body'];
+			$smtp->subject = $_POST['subject'];
 			$smtp->from = $ADMIN_EMAIL;
 			$smtp->headers = "Reply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\n";
 			
-			while ( $obj = db_rowobj($r) ) {
-				$to[] = $obj->email;
-				if ( !(++$email_block_stat%$email_block) ) {
-					$email_block_stat=0;
-					
+			while ($r = db_rowobj($c)) {
+				$to[] = $r[0];
+				if (!(++$email_block_stat % $email_block)) {
+					$email_block_stat = 0;
+
 					$smtp->to = $to;
 					$smtp->send_smtp_email();
-					
+
 					$to = array();
 				}
-				
-				if( count($to) ) {
-					$smtp->to = $to;
-					$smtp->send_smtp_email();
-				}
+			}	
+			if (count($to)) {
+				$smtp->to = $to;
+				$smtp->send_smtp_email();
 			}
 		}	
 		
 		qf($r);
 	}
 	
-	cache_buster();
-
-include('admpanel.php'); 
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>Mass Mail System</h2>
 <form method="post" name="a_frm">
@@ -93,19 +80,19 @@ include('admpanel.php');
 <table border=0 cellspacing=1 cellpadding=3>
 	<tr bgcolor="#bff8ff">
 		<td valign=top>Subject</td>
-		<td><input type="text" name="subject" value="<?php echo htmlspecialchars($subject); ?>"></td>
+		<td><input type="text" name="subject" value=""></td>
 	</tr>
 	<tr bgcolor="#bff8ff">
 		<td colspan=2 valign=top>
 			<b>Body</b><br>
-			<textarea name="body" cols=80 rows=25><?php echo htmlspecialchars($body); ?></textarea>
+			<textarea name="body" cols=80 rows=25></textarea>
 		</td>
 	</tr>
 	<tr bgcolor="#bff8ff">
 		<td colspan=2 align=right>
-			<input type="checkbox" name="ignore_override" value="1"<?php echo (($ignore_override)?' checked':''); ?>> Ignore User Override <input type="submit" value="Send" name="btn_submit">
+			<input type="checkbox" name="ignore_override" value="1"> Ignore User Override <input type="submit" value="Send" name="btn_submit">
 		</td>
 	</tr>
 </table>
 </form>
-<?php readfile('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
