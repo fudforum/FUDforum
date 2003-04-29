54097aa041d5643782e5e1aa9384ae34de4c9981
iliaa <ilia@ilia.ws>
2003-04-29 15:26:39 +0000

    Fixes.

--- install/www_root/adm/indexdb.php
+++ install/www_root/adm/indexdb.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: indexdb.php,v 1.4 2002/09/18 20:52:08 hackie Exp $
+*   $Id: indexdb.php,v 1.5 2003/04/29 15:26:39 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -15,59 +15,76 @@
 *
 ***************************************************************************/
 
-	@set_time_limit(2000);
+	@set_time_limit(2400);
 	
 	define('admin_form', 1);
 	
-	include_once "GLOBALS.php";
-	
-	fud_use('isearch.inc');
-	fud_use('fileio.inc');
-	fud_use('rev_fmt.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
 	fud_use('glob.inc', true);
+	fud_use('isearch.inc');
+	fud_use('fileio.inc');
 	
-	list($ses, $usr) = initadm();
-        
-	if( !empty($HTTP_POST_VARS['cancel']) ) {
-		header("Location: admglobal.php?"._rsidl);
-		exit;
-	}
-	include('admpanel.php');
+	require($WWW_ROOT_DISK . 'adm/admpanel.php');
 
-	if( empty($HTTP_POST_VARS['conf']) ) {
+	if (!isset($_POST['conf'])) {
 ?>		
 <form method="post" action="indexdb.php">
 <div align="center">
 This script will attempt to rebuild the search indices for the entire forum. This is a VERY CPU-intensive process 
 and can take a VERY LONG time, especially on large forums. You should ONLY run this if you absolutely must.<br><br>
 <h2>Do you wish to proceed?</h2>
-<input type="submit" name="cancel" value="No">&nbsp;&nbsp;&nbsp;<input type="submit" name="conf" value="Yes">
+<input type="submit" name="btn_cancel" value="No">&nbsp;&nbsp;&nbsp;<input type="submit" name="conf" value="Yes">
 </div>
 <?php echo _hs; ?>
 </form>
 <?php
-		readfile('admclose.html');
+		require($WWW_ROOT_DISK . 'adm/admclose.html');
 		exit;	
 	}
 	
-	if( $GLOBALS['FORUM_ENABLED'] == 'Y' ) {
+	if ($GLOBALS['FORUM_ENABLED'] == 'Y') {
 		echo '<br>Disabling the forum for the duration of maintenance run<br>';
 		maintenance_status('Undergoing maintenance, please come back later.', 'N');
 	}
 	
-	echo "<br>Please wait while index is being rebuilt.<br>This may take a while depending on the size of your forum.<br>\n";	
+	echo '<br>Please wait while index is being rebuilt.<br>This may take a while depending on the size of your forum.';
 	flush();
-	re_build_index();
 	
-	echo "Done<br>\n";
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
 	
-	if( $GLOBALS['FORUM_ENABLED'] == 'Y' ) {
+	db_lock($tbl.'search_cache WRITE, '.$tbl.'search WRITE, '.$tbl.'index WRITE, '.$tbl.'title_index WRITE, '.$tbl.'msg WRITE');
+	if (!($sid = q_singleval('SELECT MIN(query_type) FROM '.$tbl.'search_cache WHERE srch_query=\'\' AND query_type<0'))) {
+		q('DELETE FROM '.$tbl.'search');
+		q('DELETE FROM '.$tbl.'index');
+		q('DELETE FROM '.$tbl.'title_index');
+		q('DELETE FROM '.$tbl.'search_cache');
+	}
+
+	$c = uq('SELECT id, subject, length, foff, file_id FROM '.$tbl.'msg WHERE '.($sid ? ' id>'.$sid.' AND ' : '').' approved=\'Y\' ORDER BY subject');
+	$old_subject = '';
+	while ($r = db_rowarr($c)) {
+		if ($old_subject != $r[1]) {
+			$subj = $old_subject = $r[1];
+		} else {
+			$subj = '';
+		}
+		q('INSERT INTO search_cache (srch_query, query_type) (\'\', -'.$r[0].')');
+		index_text($subj, read_msg_body($r[3], $r[2], $r[4]), $r[0]);
+	}
+	qf($c);
+	un_register_fps();
+	q('DELETE FROM '.$tbl.'search_cache');
+	db_unlock();
+	
+	echo 'Done<br>';
+	
+	if ($GLOBALS['FORUM_ENABLED'] == 'Y') {
 		echo '<br>Re-enabling the forum.<br>';
 		maintenance_status($GLOBALS['DISABLED_REASON'], 'Y');
+	} else {
+		echo '<br><font size=+1 color="red">Your forum is currently disabled, to re-enable it go to the <a href="admglobal.php?'._rsidl.'">Global Settings Manager</a> and re-enable it.</font>';
 	}
-	else
-		echo '<br><font size=+1 color="red">Your forum is currently disabled, to re-enable it go to the <a href="admglobal.php?'._rsid.'">Global Settings Manager</a> and re-enable it.</font>';
 	
-	readfile('admclose.html');
+	require($WWW_ROOT_DISK . 'adm/admclose.html');
 ?>
