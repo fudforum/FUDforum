55a5db96fdd3cdc56663714740e8268675ded6f8
iliaa <ilia@ilia.ws>
2003-04-29 14:19:37 +0000

    Initial fixes.

--- install/www_root/adm/admlevel.php
+++ install/www_root/adm/admlevel.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admlevel.php,v 1.6 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admlevel.php,v 1.7 2003/04/29 14:19:37 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -17,83 +17,58 @@
 
 	define('admin_form', 1);
 	
-	include_once "GLOBALS.php";
-	
-	fud_use('widgets.inc', true);
-	fud_use('level_adm.inc', true);	
-	fud_use('util.inc');
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
-	fud_use('objutil.inc');
-	
-	list($ses, $adm) = initadm();
-	
-	cache_buster();
-	
-	$lev = new fud_level;
-	
-	if ( !empty($lev_cancel) ) {
-		header("Location: admlevel.php?"._rsidl);
-		exit();
+	fud_use('widgets.inc', true);
+
+	$tbl = $GLOBALS['DBHOST_TBL_PREFIX'];
+
+	if (isset($_POST['lev_submit'])) {
+		q('INSERT INTO '.$tbl.'level (name, img, pri, post_count) VALUES (\''.addslashes($_POST['lev_name']).'\', '.strnull(addslashes($_POST['lev_img'])).', \''.addslashes($_POST['lev_pri']).'\', '.(int)$_POST['lev_post_count'].')');
+	} else if (isset($_POST['edit'], $_POST['lev_update'])) {
+		q('UPDATE '.$tbl.'level SET 
+			name=\''.addslashes($_POST['lev_name']).'\',
+			img='.strnull(addslashes($_POST['lev_img'])).',
+			pri=\''.addslashes($_POST['lev_pri']).'\',
+			post_count='.(int)$_POST['lev_post_count'].'
+		WHERE id='.(int)$_POST['edit']);	
+	} else if (isset($_GET['edit'])) {
+		$edit = (int)$_GET['edit'];
+		list($lev_name, $lev_img, $lev_pri,$lev_post_count) = db_saq('SELECT name, img, pri, post_count FROM '.$tbl.'level WHERE id='.(int)$_GET['id']);
+	} else {
+		$edit = $lev_name = $lev_img = $lev_pri = $lev_post_count = '';
 	}
 
-	if ( @count($HTTP_POST_VARS) > 1 ) {
-		if ( !$edit ) {	
-			fetch_vars('lev_', $lev, $HTTP_POST_VARS);
-			$lev->add();
-		}
-		else if ( $edit ) {
-			$lev->get_by_id($edit);
-			fetch_vars('lev_', $lev, $HTTP_POST_VARS);
-			$lev->sync();
-		}
-		/* update levels */
-		
-		header("Location: admlevel.php?"._rsidl);
-	}
-	
-	if ( !empty($act) && $act=='del' && !empty($del_id) ) {
-		$lev->get_by_id($del_id);
-		$lev->delete();
-		/* update levels */
-
-		header("Location: admlevel.php?"._rsidl);
+	if (isset($_GET['del'])) {
+		q('DELETE FROM '.$tbl.'level WHERE id='.(int)$_GET['del']);
 	}
 
-	if ( !empty($edit) && !(@count($HTTP_POST_VARS)>1) ) {
-		$lev->get_by_id($edit);
-		export_vars('lev_', $lev);
-	}
-	
-	if( !empty($rebuild_levels) ) {
-		db_lock($GLOBALS['DBHOST_TBL_PREFIX'].'users+, '.$GLOBALS['DBHOST_TBL_PREFIX'].'level+');
-
+	if (isset($_GET['rebuild_levels'])) {
+		db_lock($tbl.'users WRITE, '.$tbl.'level WRITE');
 		$pl = 2000000000;
-		$r = q("SELECT id,post_count,name FROM ".$GLOBALS['DBHOST_TBL_PREFIX']."level ORDER BY post_count DESC");
-		while( $obj = db_rowobj($r) ) {
-			q("UPDATE ".$GLOBALS['DBHOST_TBL_PREFIX']."users SET level_id=".$obj->id." WHERE posted_msg_count<".$pl." AND posted_msg_count>=".$obj->post_count);
-			$pl = $obj->post_count;
+		$c = q('SELECT id, post_count FROM '.$tbl.'level ORDER BY post_count DESC');
+		while ($r = db_rowarr($c)) {
+			q('UPDATE '.$tbl.'users SET level_id='.$r[0].' WHERE posted_msg_count<'.$pl.' AND posted_msg_count>='.$r[1]);
+			$pl = $r[1];
 		}
 		qf($r);
 		db_unlock();
 	}
 
-	$img_path = '../images/';
-	
-	include('admpanel.php'); 
+	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
 ?>
 <h2>Rank Manager</h2>
 <div align="center"><font size="+1" color="#ff0000">If you've made any modification to the user ranks<br>YOU MUST RUN CACHE REBUILDER by &gt;&gt; <a href="admlevel.php?rebuild_levels=1&<?php echo _rsid; ?>">clicking here</a> &lt;&lt;</font></div>
-<form method="post" name="lev_form">
+<form method="post" name="lev_form" action="admlevel.php">
 <?php echo _hs; ?>
 <table border=0 cellspacing=1 cellpadding=2>
 	<tr bgcolor="#bff8ff">
 		<td>Rank Name</td>
-		<td><input type="text" name="lev_name" value="<?php echo (!isset($lev_name)?'':htmlspecialchars($lev_name)); ?>"></td>
+		<td><input type="text" name="lev_name" value="<?php echo htmlspecialchars($lev_name); ?>"></td>
 	</tr>
 	<tr bgcolor="#bff8ff">
-		<td>Rank Image</td>
-		<td><input type="text" name="lev_img" value="<?php echo (!isset($lev_img)?'':htmlspecialchars($lev_img)); ?>"><br>
-		<font size=-1>Your image path is: <a href="#" onClick="javascript: document.lev_form.lev_img.value='<?php echo $img_path; ?>'+document.lev_form.lev_img.value; "><?php echo $img_path; ?></a></font></td>
+		<td>Rank Image<br><font size="-1">URL to the image<font></td>
+		<td><input type="text" name="lev_img" value="<?php echo htmlspecialchars($lev_img); ?>"><br>
 	</tr>
 	
 	<tr bgcolor="#bff8ff">
@@ -103,26 +78,23 @@
 	
 	<tr bgcolor="#bff8ff">
 		<td>Post Count</td>
-		<td><input type="text" name="lev_post_count" value="<?php echo (!isset($lev_post_count)?'':$lev_post_count); ?>" size=11 maxLength=10></td>
+		<td><input type="text" name="lev_post_count" value="<?php echo $lev_post_count; ?>" size=11 maxLength=10></td>
 	</tr>
 	
 	<tr>
 		<td colspan=2 bgcolor="#bff8ff" align=right>
-		<?php
-			if ( empty($edit) ) {
+<?php
+			if (!$edit) {
 				echo '<input type="submit" name="lev_submit" value="Add Level">';
+			} else {
+				echo '<input type="submit" name="lev_cancel" value="Cancel"> <input type="submit" name="lev_update" value="Update">';
 			}
-			else {
-				echo '<input type="submit" name="lev_cancel" value="Cancel"> ';
-				echo '<input type="submit" name="lev_update" value="Update">';
-			}
-		?>
+?>
 		</td>
 	</tr>
 </table>
 </form>
 
-<?php $lev->get_all_levels(); ?>
 <table border=0 cellspacing=1 cellpadding=1>
 <tr bgcolor="#e5ffe7">
 	<td>Name</td>
@@ -130,14 +102,17 @@
 	<td>Action</td>
 </tr>
 <?php
-
-	$lev->resetlev();
-	$i=1;
-	while ( $obj=$lev->nextlev() ) {
-		$bgcolor = (($i++)%2) ? ' bgcolor="#fffee5"':'';
-		if ( !empty($edit) && $edit == $obj->id ) $bgcolor=' bgcolor="#ffb5b5"';
-		echo "<tr$bgcolor><td>$obj->name</td><td align=center>$obj->post_count</td><td><a href=\"admlevel.php?edit=$obj->id&"._rsid."\">Edit</a> | <a href=\"admlevel.php?act=del&del_id=$obj->id&"._rsid."\">Delete</a></td></tr>\n";
+	$c = uq('SELECT * FROM '.$tbl.'level ORDER BY post_count');
+	$i = 1;
+	while ($r = db_rowobj($c)) {
+		if ($edit == $r->id) {
+			$bgcolor = ' bgcolor="#ffb5b5"';
+		} else {
+			$bgcolor = ($i++%2) ? ' bgcolor="#fffee5"' : '';
+		}
+		echo '<tr'.$bgcolor.'><td>'.$r->name.'</td><td align=center>'.$r->post_count.'</td><td><a href="admlevel.php?edit='.$r->id.'&'._rsidl.'">Edit</a> | <a href="admlevel.php?del='.$r->id.'&'._rsidl.'">Delete</a></td></tr>';
 	}
+	qf($c);
 ?>
 </table>
-<?php require('admclose.html'); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
