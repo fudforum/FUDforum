fda945f29987b4c7852b90237aa6599bb5760f17
iliaa <ilia@ilia.ws>
2003-04-29 01:14:42 +0000

    Fixes.

--- install/www_root/adm/admsysinfo.php
+++ install/www_root/adm/admsysinfo.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admsysinfo.php,v 1.4 2002/09/18 20:52:08 hackie Exp $
+*   $Id: admsysinfo.php,v 1.5 2003/04/29 01:14:42 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -16,9 +16,7 @@
 ***************************************************************************/
 
 	define('admin_form', 1);
-	
-	include_once "GLOBALS.php";
-	
+	require('GLOBALS.php');
 	fud_use('adm.inc', true);
 
 function get_php_setting($val)
@@ -28,37 +26,26 @@ function get_php_setting($val)
 
 function get_server_software()
 {
-	if( !empty($GLOBALS['HTTP_ENV_VARS']['SERVER_SOFTWARE']) ) 
-		return $GLOBALS['HTTP_ENV_VARS']['SERVER_SOFTWARE'];
-	else if( !empty($GLOBALS['HTTP_SERVER_VARS']['SERVER_SOFTWARE']) ) 
-		return $GLOBALS['HTTP_SERVER_VARS']['SERVER_SOFTWARE'];
-	else
-		return 'n/a';	
+	if (isset($_SERVER['SERVER_SOFTWARE'])) {
+		return $_SERVER['SERVER_SOFTWARE'];
+	} else if (($sf = getenv('SERVER_SOFTWARE'))) {
+		return $sf;
+	} else {
+		return 'n/a';
+	}
 }
 
-function get_os()
-{
-	if( function_exists("php_uname") ) 
-		return php_uname();
-	else if( ($ret = exec("uname -a")) ) 
-		return $ret;
-	else
-		return 'unknown';	
-}
-	
-	list($ses, $usr) = initadm();
-
-	include "admpanel.php";
+	require($WWW_ROOT_DISK . 'adm/admpanel.php');
 ?>
 <h2>System Configuration</h2>
 <table cellspacing=3 cellpadding=1 border=0>
 <tr>
 	<td><b>PHP built On:</b></td>
-	<td><?php echo get_os(); ?></td>
+	<td><?php echo php_uname(); ?></td>
 </tr>
 <tr>
-	<td><b>MySQL Version:</b></td>
-	<td><?php echo q_singleval("SELECT VERSION()"); ?></td>
+	<td><b>Database Version:</b></td>
+	<td><?php echo q_singleval('SELECT VERSION()'); ?></td>
 </tr>
 <tr>
 	<td><b>PHP Version:</b></td>
@@ -82,38 +69,42 @@ function get_os()
 		<table cellspacing=1 cellpadding=1 border=0>
 			<tr>
 				<td>Safe Mode:</td>
-				<td><?php echo get_php_setting("safe_mode"); ?></td>
+				<td><?php echo get_php_setting('safe_mode'); ?></td>
+			</tr>
+			<tr>
+				<td>Open basedir:</td>
+				<td><?php echo (($ob = ini_get('open_basedir')) ? $df : 'none'); ?></td>
 			</tr>
 			<tr>
 				<td>Display Errors:</td>
-				<td><?php echo get_php_setting("display_errors"); ?></td>
+				<td><?php echo get_php_setting('display_errors'); ?></td>
 			</tr>
 			<tr>
 				<td>File Uploads:</td>
-				<td><?php echo get_php_setting("file_uploads"); ?></td>
+				<td><?php echo get_php_setting('file_uploads'); ?></td>
 			</tr>
 			<tr>
 				<td>Magic Quotes:</td>
-				<td><?php echo get_php_setting("magic_quotes_gpc"); ?></td>
+				<td><?php echo get_php_setting('magic_quotes_gpc'); ?></td>
 			</tr>
 			<tr>
 				<td>Register Globals:</td>
-				<td><?php echo get_php_setting("register_globals"); ?></td>
+				<td><?php echo get_php_setting('register_globals'); ?></td>
 			</tr>
 			<tr>
 				<td>Output Buffering:</td>
-				<td><?php echo get_php_setting("output_buffering"); ?></td>
+				<td><?php echo get_php_setting('output_buffering'); ?></td>
 			</tr>
 			<tr>
 				<td>Disabled Functions:</td>
-				<td><?php echo (($df=ini_get("disable_functions"))?$df:'none'); ?></td>
+				<td><?php echo (($df=ini_get('disable_functions'))?$df:'none'); ?></td>
 			</tr>
 			<tr>
 				<td>PSpell Support:</td>
-				<td><?php echo function_exists("pspell_new") ? 'Yes' : 'No'; ?></td>
+				<td><?php echo function_exists('pspell_new') ? 'Yes' : 'No'; ?></td>
 			</tr>
 		</table>
 	</td>
 </tr>
 </table>
-<?php readfile("admclose.html"); ?>
+<?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
