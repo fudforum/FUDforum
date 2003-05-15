7031f7d1acb6a8990bc3d7af76c022eed2b16f8e
iliaa <ilia@ilia.ws>
2003-05-15 18:22:40 +0000

    RDF settings

---
+++ install/forum_data/include/RDF.php
@@ -0,0 +1,22 @@
+<?php
+/***************************************************************************
+*   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
+*   email                : forum@prohost.org
+*
+*   $Id: RDF.php,v 1.1 2003/05/15 18:22:40 hackie Exp $
+****************************************************************************
+          
+****************************************************************************
+*
+*	This program is free software; you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation; either version 2 of the License, or
+*	(at your option) any later version.
+*
+***************************************************************************/
+
+	$RDF_ENABLED	= "Y";		/* boolean */
+	$AUTH 		= "N";		/* boolean */
+	$AUTH_ID	= "0";		/* 0 - treat as anon user, >0 treat like specific forum user */
+	$MAX_N_RESULTS	= "100";	/* int */
+?>
