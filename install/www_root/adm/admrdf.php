3a8cb9eb543b0d1f6372e7a8978a0faa0ba61015
iliaa <ilia@ilia.ws>
2003-05-16 14:27:49 +0000

    Added docs regarding RDF feed

--- install/www_root/adm/admrdf.php
+++ install/www_root/adm/admrdf.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: admrdf.php,v 1.1 2003/05/15 18:21:34 hackie Exp $
+*   $Id: admrdf.php,v 1.2 2003/05/16 14:27:49 hackie Exp $
 ****************************************************************************
           
 ****************************************************************************
@@ -59,6 +59,8 @@ function print_string_field($descr, $help, $field, $is_int=0)
 	}
 
 	require($WWW_ROOT_DISK . 'adm/admpanel.php'); 
+
+	$rdf_url = $WWW_ROOT . 'rdf.php';
 ?>
 <h2>RDF Feed Configuration</h2>
 <form method="post" action="admrdf.php">
@@ -73,4 +75,57 @@ function print_string_field($descr, $help, $field, $is_int=0)
 </table>
 <input type="hidden" name="form_posted" value="1">
 </form>
+<br>
+<table border=0 cellspacing=1 cellpadding=3>
+<tr><th><b>Quick RDF Tutorial</b></th></tr>
+<tr><td bgcolor="#fffee5">
+If enabled, the RDF stream for your forum can be found at: <a href="<?php echo $rdf_url; ?>" target="_blank"><?php echo $rdf_url; ?></a><br />
+The streams has three modes of operation, which are tailored for a specific data that you wish to fetch. You
+can specify the mode by passing the 'mode' parameter via GET to the rdf script. The support modes are 'u', for 
+user information retrieval, 't' for topic retrieval and 'm' for message retrieval. Each mode has a number of
+options that allow you to specify the exact nature of the data you wish to fetch. Below is the explanation of 
+those flags, it should be noted that flags are not exclusive and you can use as many as you like in a single
+request.<br /><br />
+<h4><u><b>'m' mode (messages)</b></u></h4>
+<blockquote>
+	<table border=0 cellspacing=1 cellpadding=3>
+		<tr><td><i>cat<i></td><td>Only retrieve messages from category with this id.</td></tr>
+		<tr><td><i>frm<i></td><td>Only retrieve messages from forum with this id.</td></tr>
+		<tr><td><i>th<i></td><td>Only retrieve messages from topic with this id.</td></tr>
+		<tr><td><i>id<i></td><td>Retrieve a single message with the specified id.</td></tr>
+		<tr><td><i>ds<i></td><td>Only retrieve messages posted after the specified date (unix timestamp).</td></tr>
+		<tr><td><i>de<i></td><td>Only retrieve messages posted before the specified date (unix timestamp).</td></tr>
+		<tr><td><i>n</i></td><td>Fetch no more then <i>n</i> messages (cannot be higher then overall maximum).</td></tr>
+		<tr><td><i>o</i></td><td>Starting offset from which to begin fetching messages.</td></tr>
+		<tr><td><i>l</i></td><td>Order messages from newest to oldest.</td></tr>
+	</table>
+</blockquote>
+<h4><u><b>'t' mode (topics)</b></u></h4>
+<blockquote>
+	<table border=0 cellspacing=1 cellpadding=3>
+		<tr><td><i>cat<i></td><td>Only retrieve topics from category with this id.</td></tr>
+		<tr><td><i>frm<i></td><td>Only retrieve topics from forum with this id.</td></tr>
+		<tr><td><i>id<i></td><td>Retrieve a single topic with the specified id.</td></tr>
+		<tr><td><i>ds<i></td><td>Only retrieve topics where the last message was posted after the specified date (unix timestamp).</td></tr>
+		<tr><td><i>de<i></td><td>Only retrieve topics where the last message was posted before the specified date (unix timestamp).</td></tr>
+		<tr><td><i>n</i></td><td>Fetch no more then <i>n</i> topics (cannot be higher then overall maximum).</td></tr>
+		<tr><td><i>o</i></td><td>Starting offset from which to begin fetching topics.</td></tr>
+		<tr><td><i>l</i></td><td>Order topics from newest to oldest.</td></tr>
+	</table>
+</blockquote>
+<h4><u><b>'u' mode (topics)</b></u></h4>
+<blockquote>
+	<table border=0 cellspacing=1 cellpadding=3>
+		<tr><td><i>pc</i></td><td>Order users by number of messages posted, from largest to smallest.</td></tr>
+		<tr><td><i>rd</i></td><td>Order users by registration, from most recent to oldest.</td></tr>
+		<tr><td><i>cl</i></td><td>Only show users who are currently online.</td></tr>
+		<tr><td><i>n</i></td><td>Fetch no more then <i>n</i> users (cannot be higher then overall maximum).</td></tr>
+		<tr><td><i>o</i></td><td>Starting offset from which to begin fetching users.</td></tr>
+	</table>
+</blockquote>
+<b>Example:</b><br />
+The following link will fetch 10 most recent messages from your forum:<br />
+<a href="<?php echo $rdf_url; ?>?mode=m&l=1&n=10" target="_blank"><?php echo $rdf_url; ?>?mode=m&l=1&n=10</a>
+</td></tr>
+</table>
 <?php require($WWW_ROOT_DISK . 'adm/admclose.html'); ?>
