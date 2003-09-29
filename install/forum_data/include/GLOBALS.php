80ab69ea40abc93b0e90029abba7ca9242bec477
iliaa <ilia@ilia.ws>
2003-09-29 22:24:27 +0000

    Global settings (start work)

--- install/forum_data/include/GLOBALS.php
+++ install/forum_data/include/GLOBALS.php
@@ -6,15 +6,11 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-****************************************************************************
-          
-****************************************************************************
-*
-*	FUDforum Copyright (C) 2001,2002 Advanced Internet Designs Inc.
-*	This program is free software; you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation; either version 2 of the License, or
-*	(at your option) any later version.
+* FUDforum Copyright (c) 2001-2003 Advanced Internet Designs Inc.
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version. 
 *
 ***************************************************************************/
 
@@ -28,124 +24,84 @@
 	$FILE_STORE		= "";
 	$FORUM_SETTINGS_PATH 	= "";
 
-	$CUSTOM_AVATARS		= "ALL";	/* enum(OFF, BUILT, URL, UPLOAD, BUILT_URL, BUILT_UPLOAD, URL_UPLOAD, ALL) */
-	$CUSTOM_AVATAR_MAX_SIZE = "10000";	/* bytes */
+	$FUD_OPT_1		= 1744762047;
+	$FUD_OPT_2		= 695676991;
+
+	$CUSTOM_AVATAR_MAX_SIZE = 10000;	/* bytes */
 	$CUSTOM_AVATAR_MAX_DIM	= "64x64";	/* width x height (pixels) */
-	$CUSTOM_AVATAR_APPOVAL  = "Y";		/* boolean */
-	$AVATAR_ALLOW_SWF	= "N";		/* boolean */
 
 	$COOKIE_PATH		= "";
 	$COOKIE_DOMAIN		= "";
 	$COOKIE_NAME		= "";
-	$COOKIE_TIMEOUT 	= "604800";	/* seconds */
-	$SESSION_TIMEOUT 	= "1800";	/* seconds */
-	$SESSION_USE_URL	= "Y";		/* boolean */
+	$COOKIE_TIMEOUT 	= 604800;	/* seconds */
+	$SESSION_TIMEOUT 	= 1800;		/* seconds */
 
 	$DBHOST 		= "";
 	$DBHOST_USER		= "";
-	$DBHOST_PASSWORD		= "";
+	$DBHOST_PASSWORD	= "";
 	$DBHOST_DBNAME		= ""; 
-	$DBHOST_PERSIST		= "N";     	/* boolean */
-	$DBHOST_TBL_PREFIX	= "fud2_";	/* do not modify this */
+	$DBHOST_TBL_PREFIX	= "fud2_";		/* do not modify this */
 
-	$USE_SMTP		= "N";		/* boolean */
 	$FUD_SMTP_SERVER	= "";
-	$FUD_SMTP_TIMEOUT	= "10";		/* seconds */
+	$FUD_SMTP_TIMEOUT	= 10;		/* seconds */
 	$FUD_SMTP_LOGIN		= "";
 	$FUD_SMTP_PASS		= "";
 
 	$ADMIN_EMAIL 		= "";
 
-	$PM_ENABLED		= "Y";		/* boolean */
-	$PRIVATE_ATTACHMENTS	= "5";		/* int */
-	$PRIVATE_ATTACH_SIZE	= "1000000";	/* bytes */
-	$PRIVATE_TAGS		= "ML";		/* toggle N:ML:HTML */
-	$PRIVATE_MSG_SMILEY	= "Y";		/* boolean */
-	$PRIVATE_IMAGES		= "Y";		/* boolean */
-	$MAX_PMSG_FLDR_SIZE	= "300000";	/* bytes */
+	$PRIVATE_ATTACHMENTS	= 5;		/* int */
+	$PRIVATE_ATTACH_SIZE	= 1000000;	/* bytes */
+	$MAX_PMSG_FLDR_SIZE	= 300000;	/* bytes */
 
-	$ALLOW_SIGS		= "Y";		/* boolean */
-	$FORUM_CODE_SIG		= "ML";		/* toggle N:ML:HTML */
-	$FORUM_SML_SIG		= "Y";		/* boolean */
-	$FORUM_IMG_SIG		= "Y";		/* boolean */
-	$FORUM_IMG_CNT_SIG	= "2";		/* int */
+	$FORUM_IMG_CNT_SIG	= 2;		/* int */
+	$FORUM_SIG_ML		= 256;		/* int */
 
-	$FORUM_ENABLED		= "Y";		/* boolean */
+	$UNCONF_USER_EXPIRY	= 7;		/* days */
+	$MOVED_THR_PTR_EXPIRY	= 3;		/* days */
 
-	$UNCONF_USER_EXPIRY	= "7";		/* days */
-	$MOVED_THR_PTR_EXPIRY	= "3";		/* days */
-
-	$USE_ALIASES		= "N";		/* boolean */
-	$MULTI_HOST_LOGIN	= "N";		/* boolean */
-	$MAX_SMILIES_SHOWN	= "15"; 	/* int */
-	$ALLOW_REGISTRATION	= "Y";		/* boolean */
-	$EMAIL_CONFIRMATION	= "Y";		/* boolean */
-	$SPELL_CHECK_ENABLED	= "Y";		/* boolean */
-	$PUBLIC_RESOLVE_HOST	= "N";		/* boolean */
-	$ACTION_LIST_ENABLED	= "Y";		/* boolean */
+	$MAX_SMILIES_SHOWN	= 15;		/* int */
 	$DISABLED_REASON	= "Temporarily offline; please come back soon!";
-	$COPPA			= "Y"; 		/* boolean */
-	$POSTS_PER_PAGE 	= "40";
-	$THREADS_PER_PAGE	= "40";
-	$WORD_WRAP		= "60";
+	$POSTS_PER_PAGE 	= 40;
+	$THREADS_PER_PAGE	= 40;
+	$WORD_WRAP		= 60;
 	$NOTIFY_FROM		= "";		/* email */
-	$NOTIFY_WITH_BODY	= "N";		/* boolean */
 	$ANON_NICK		= "Anonymous Coward";
-	$FLOOD_CHECK_TIME	= "60";		/* seconds */
-	$ALLOW_EMAIL		= "Y";		/* boolean */
+	$FLOOD_CHECK_TIME	= 60;		/* seconds */
 	$SERVER_TZ		= "America/Montreal"; /* timezone code from tz.inc */
-	$MEMBER_SEARCH_ENABLED	= "Y";		/* boolean */
-	$FORUM_SEARCH		= "Y";		/* boolean */
-	$SEARCH_CACHE_EXPIRY	= "172800";	/* seconds */
-	$MEMBERS_PER_PAGE	= "40";
-	$POLLS_PER_PAGE		= "40";
-	$THREAD_MSG_PAGER	= "5";
-	$GENERAL_PAGER_COUNT	= "15";
-	$SHOW_EDITED_BY		= "Y";		/* boolean */
-	$EDITED_BY_MOD		= "Y";		/* boolean */
-	$EDIT_TIME_LIMIT	= "0";
-	$DISPLAY_IP		= "N";		/* boolean */
-	$LOGEDIN_TIMEOUT	= "5";		/* minutes */
-	$MAX_IMAGE_COUNT	= "10";
-	$LOGEDIN_LIST		= "Y";		/* boolean */
-	$STATS_CACHE_AGE	= "600";	/* seconds */
-	$PUBLIC_STATS		= "Y";		/* boolean */
+	$SEARCH_CACHE_EXPIRY	= 172800;	/* seconds */
+	$MEMBERS_PER_PAGE	= 40;
+	$POLLS_PER_PAGE		= 40;
+	$THREAD_MSG_PAGER	= 5;
+	$GENERAL_PAGER_COUNT	= 15;
+	$EDIT_TIME_LIMIT	= 0;
+	$LOGEDIN_TIMEOUT	= 5;		/* minutes */
+	$MAX_IMAGE_COUNT	= 10;
+	$STATS_CACHE_AGE	= 600;		/* seconds */
 	$FORUM_TITLE		= "";
 	$SITE_HOME_PAGE		= "";
-	$MAX_LOGIN_SHOW		= "25";
-	$MAX_LOCATION_SHOW	= "25";
-	$DEFAULT_THREAD_VIEW 	= "msg";    	/* toggle msg:tree:tree_msg:msg_tree */
-	$SHOW_N_MODS		= "2";
-	$ENABLE_AFFERO		= "N";		/* boolean */
+	$MAX_LOGIN_SHOW		= 25;
+	$MAX_LOCATION_SHOW	= 25;
+	$SHOW_N_MODS		= 2;
 
-	$TREE_THREADS_ENABLE	= "N";		/* boolean */
-	$TREE_THREADS_MAX_DEPTH	= "15";
-	$TREE_THREADS_MAX_SUBJ_LEN = "75";
+	$TREE_THREADS_MAX_DEPTH	= 15;
+	$TREE_THREADS_MAX_SUBJ_LEN = 75;
 
-	$FORUM_INFO		= "Y";		/* boolean */
-	$ONLINE_OFFLINE_STATUS	= "Y";		/* boolean */
-	$FILE_LOCK		= "N";		/* boolean */
-	$MODERATE_USER_REGS	= "N";		/* boolean */
-	$NEW_ACCOUNT_NOTIFY	= "Y";		/* boolean */
-	$REG_TIME_LIMIT		= "60";		/* seconds */
-	$ENABLE_THREAD_RATING	= "Y";		/* boolean */
-	$TRACK_REFERRALS	= "Y";		/* boolean */
-	$POST_ICONS_PER_ROW	= "9";		/* int */
-	$MAX_LOGGEDIN_USERS	= "25";		/* int */
-	$USE_PATH_INFO		= "N";		/* boolean */
-	$PHP_COMPRESSION_ENABLE	= "N";		/* boolean */
-	$PHP_COMPRESSION_LEVEL	= "9";		/* int 1-9 */
-	$ALLOW_PROFILE_IMAGE	= "Y";		/* boolean */
-	$MODERATED_POST_NOTIFY	= "Y";		/* boolean */
-	$BUST_A_PUNK		= "N";		/* boolean */
-	$MNAV_MAX_DATE		= "31";		/* days */
-	$MNAV_MAX_LEN		= "256";	/* characters */
-	$SHOW_XML_LINK		= "Y";		/* boolean */
-	$SHOW_PDF_LINK		= "Y";		/* boolean */
-	$DWLND_REF_CHK		= "N";		/* boolean */
-/* 
- * DO NOT EDIT FILE BEYOND THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING
- */
+	$REG_TIME_LIMIT		= 60;		/* seconds */
+	$POST_ICONS_PER_ROW	= 9;		/* int */
+	$MAX_LOGGEDIN_USERS	= 25;		/* int */
+	$PHP_COMPRESSION_LEVEL	= 9;		/* int 1-9 */
+	$MNAV_MAX_DATE		= 31;		/* days */
+	$MNAV_MAX_LEN		= 256;		/* characters */
+
+	$AUTH_ID		= 0;		/* 0 - treat as anon user, >0 treat like specific forum user */
+	$MAX_N_RESULTS		= 100;		/* int */
+
+	$PDF_PAGE		= "letter";	/* string */
+	$PDF_WMARGIN		= 15;		/* int */
+	$PDF_HMARGIN		= 15;		/* int */
+	$PDF_MAX_CPU		= 60;		/* seconds */
+
+/* DO NOT EDIT FILE BEYOND THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING */
 
 	require($INCLUDE.'core.inc');
 ?>
