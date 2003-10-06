e13237d2a000a430606ae58bedf920653532e4c3
iliaa <ilia@ilia.ws>
2003-10-06 19:08:08 +0000

    Default insert cleanup.

--- install/forum_data/sql/def_cat.sql
+++ install/forum_data/sql/def_cat.sql
@@ -1,1 +1,1 @@
-INSERT INTO {SQL_TABLE_PREFIX}cat VALUES (1, 'Test Category', ' - Just a test category', 1|2, 1);
+INSERT INTO {SQL_TABLE_PREFIX}cat (name,description,cat_opt,view_order) VALUES ('Test Category', ' - Just a test category', 1|2, 1);
--- install/forum_data/sql/def_levels.sql
+++ install/forum_data/sql/def_levels.sql
@@ -1,3 +1,3 @@
-INSERT INTO {SQL_TABLE_PREFIX}level (id,name,post_count) VALUES (1,'Senior Member',100);
-INSERT INTO {SQL_TABLE_PREFIX}level (id,name,post_count) VALUES (2,'Member',30);
-INSERT INTO {SQL_TABLE_PREFIX}level (id,name,post_count) VALUES (3,'Junior Member',0);
+INSERT INTO {SQL_TABLE_PREFIX}level (name,post_count) VALUES ('Senior Member',100);
+INSERT INTO {SQL_TABLE_PREFIX}level (name,post_count) VALUES ('Member',30);
+INSERT INTO {SQL_TABLE_PREFIX}level (name,post_count) VALUES ('Junior Member',0);
