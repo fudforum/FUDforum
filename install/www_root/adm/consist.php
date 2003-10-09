897ddf71c6186a7d58de49d18199ffd650c479f8
iliaa <ilia@ilia.ws>
2003-10-09 14:29:12 +0000

    Fixes.

--- install/www_root/adm/consist.php
+++ install/www_root/adm/consist.php
@@ -3,7 +3,7 @@
 *   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
 *   email                : forum@prohost.org
 *
-*   $Id: consist.php,v 1.61 2003/10/06 18:45:37 hackie Exp $
+*   $Id: consist.php,v 1.62 2003/10/09 14:29:12 hackie Exp $
 ****************************************************************************
 
 ****************************************************************************
@@ -125,7 +125,7 @@ forum will be disabled.<br><br>
 	$tbls = get_fud_table_list();
 	// add the various table aliases
 	array_push($tbls, 	$tbl.'users u', $tbl.'forum f', $tbl.'thread t', $tbl.'poll p', $tbl.'poll_opt po', $tbl.'poll_opt_track pot',
-				$tbl.'msg m', $tbl.'pmsg pm', $tbl.'mod mm', $tbl.'thread_rate_track trt', $tbl.'msg_report mr', $tbl.'fc_view',
+				$tbl.'msg m', $tbl.'pmsg pm', $tbl.'mod mm', $tbl.'thread_rate_track trt', $tbl.'msg_report mr', $tbl.'cat c',
 				$tbl.'forum_notify fn', $tbl.'thread_notify tn', $tbl.'buddy b', $tbl.'user_ignore i', $tbl.'msg m1', $tbl.'msg m2',
 				$tbl.'users u1', $tbl.'users u2', $tbl.'attach a', $tbl.'thr_exchange te', $tbl.'read r', $tbl.'mime mi',
 				$tbl.'group_members gm', $tbl.'group_resources gr', $tbl.'groups g', $tbl.'group_members gm1', $tbl.'group_members gm2', $tbl.'themes thm');
@@ -405,7 +405,7 @@ forum will be disabled.<br><br>
 	$dp = opendir($WWW_ROOT_DISK . 'images/smiley_icons');
 	readdir($dp); readdir($dp);
 	while ($f = readdir($dp)) {
-		if (!isset($sml[$f]) && !preg_match('!\.(gif|png|jpg|jpeg)$!i', $f)) {
+		if (!isset($sml[$f]) && !preg_match('!\.(gif|png|jpg|jpeg|html)$!i', $f)) {
 			if (@unlink($WWW_ROOT_DISK . 'images/smiley_icons/' . $f)) {
 				draw_stat('deleted smiley: ' . $f);
 				++$cnt;
