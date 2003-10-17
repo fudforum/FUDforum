6145afdc1433b6d6e4e5ad85a11d00ceaac11dc6
iliaa <ilia@ilia.ws>
2003-10-17 03:49:22 +0000

    Approve automatic users.

--- install/forum_data/sql/def_forum.sql
+++ install/forum_data/sql/def_forum.sql
@@ -4,7 +4,7 @@ INSERT INTO {SQL_TABLE_PREFIX}groups (name, forum_id, groups_opt) VALUES('Global
 INSERT INTO {SQL_TABLE_PREFIX}groups (name, forum_id, groups_opt) VALUES('Global Registered Access', 0, 1|2|4|8|128|256|512|1024|16384|32768|262144);
 INSERT INTO {SQL_TABLE_PREFIX}groups (name, forum_id, groups_opt) VALUES ('TestForum', 1, 1|2|4|8|16|32|64|128|256|512|1024|2048|4096|8192|16384|32768|262144);
 INSERT INTO {SQL_TABLE_PREFIX}group_resources (group_id, resource_id) VALUES(3, 1);
-INSERT INTO {SQL_TABLE_PREFIX}group_members (user_id, group_id, group_members_opt) VALUES (0, 3, 1|2|262144);
-INSERT INTO {SQL_TABLE_PREFIX}group_members (user_id, group_id, group_members_opt) VALUES (2147483647, 3, 1|2|4|8|128|256|512|1024|16384|32768|262144);
+INSERT INTO {SQL_TABLE_PREFIX}group_members (user_id, group_id, group_members_opt) VALUES (0, 3, 1|2|262144|65536);
+INSERT INTO {SQL_TABLE_PREFIX}group_members (user_id, group_id, group_members_opt) VALUES (2147483647, 3, 1|2|4|8|128|256|512|1024|16384|32768|262144|65536);
 INSERT INTO {SQL_TABLE_PREFIX}group_cache (user_id, resource_id, group_id, group_cache_opt) VALUES (0, 1, 3, 1|2|262144);
 INSERT INTO {SQL_TABLE_PREFIX}group_cache (user_id, resource_id, group_id, group_cache_opt) VALUES (2147483647, 1, 3, 1|2|4|8|128|256|512|1024|16384|32768|262144);
