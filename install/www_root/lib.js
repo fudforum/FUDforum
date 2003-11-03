edcbf8dba0528ba547d5cb69cedd796cb466c48c
iliaa <ilia@ilia.ws>
2003-11-03 18:52:00 +0000

    Update license.

--- install/www_root/lib.js
+++ install/www_root/lib.js
@@ -1,17 +1,12 @@
 /***************************************************************************
-*   copyright            : (C) 2001,2002 Advanced Internet Designs Inc.
-*   email                : forum@prohost.org
+* copyright            : (C) 2001-2003 Advanced Internet Designs Inc.
+* email                : forum@prohost.org
+* $Id$
 *
-*   $Id$
-****************************************************************************
-          
-****************************************************************************
-*
-*	This program is free software; you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation; either version 2 of the License, or
-*	(at your option) any later version.
-*
+* This program is free software; you can redistribute it and/or modify it 
+* under the terms of the GNU General Public License as published by the 
+* Free Software Foundation; either version 2 of the License, or 
+* (at your option) any later version.
 ***************************************************************************/
 
 JS_HELPOFF = false;
