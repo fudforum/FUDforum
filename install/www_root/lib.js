56106f42b122615e6f818e056dc86c539b0dd01d
iliaa <ilia@ilia.ws>
2003-11-09 19:32:21 +0000

    Switched from gif to png images for icons.

--- install/www_root/lib.js
+++ install/www_root/lib.js
@@ -175,3 +175,27 @@ function layerVis(layer,on)
 			document.all[layer].style.visibility = "hidden";
 	}
 }
+
+// This code below is used to make PNG's to work in IE.
+// The code comes from youngpup.net. Thanks youngpup!
+
+if (navigator.platform == "Win32" && navigator.appName == "Microsoft Internet Explorer" && window.attachEvent) {
+	document.writeln('<style type="text/css">img { visibility:hidden; } </style>');
+	window.attachEvent("onload", fnLoadPngs);
+}
+
+function fnLoadPngs() {
+	var rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
+	var itsAllGood = (rslt != null && Number(rslt[1]) >= 5.5);
+
+	for (var i = document.images.length - 1, img = null; (img = document.images[i]); i--) {
+		if (itsAllGood && img.src.match(/\.png$/i) != null) {
+			var src = img.src;
+			img.style.width = img.width + "px";
+			img.style.height = img.height + "px";
+			img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='scale')"
+			img.src = "blank.gif";
+		}
+		img.style.visibility = "visible";
+	}
+}
