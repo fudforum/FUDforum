a3124c0518f46c2e45bdfb4a82b86b255f591e3e
iliaa <ilia@ilia.ws>
2003-11-12 13:45:18 +0000

    More permissions stuff.

--- install/www_root/adm/admlock.php
+++ install/www_root/adm/admlock.php
@@ -2,7 +2,7 @@
 /***************************************************************************
 * copyright            : (C) 2001-2003 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admlock.php,v 1.22 2003/10/09 14:34:32 hackie Exp $
+* $Id: admlock.php,v 1.23 2003/11/12 13:45:18 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it 
 * under the terms of the GNU General Public License as published by the 
@@ -41,8 +41,13 @@ function chmoddir($dirn, $dirp, $filep, $rec=false)
 			@unlink($ERROR_PATH.'FILE_LOCK');
 			$FUD_OPT_2 ^= 8388608;
 		} else {
-			$dirperms = 0700;
-			$fileperms = 0600;
+			if (!strncmp(PHP_SAPI, 'apache', 6)) {
+				$dirperms = 0700;
+				$fileperms = 0600;
+			} else {
+				$dirperms = 0711;
+				$fileperms = 0644;
+			}
 		}
 
 		chmoddir(realpath($WWW_ROOT_DISK), $dirperms, $fileperms, true);
