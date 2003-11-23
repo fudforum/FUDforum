ddfd381316420180d6d7045a9d29acc62a8d18d9
iliaa <ilia@ilia.ws>
2003-11-23 12:18:04 +0000

    Trim message prior to writing.

--- install/www_root/adm/msglist.php
+++ install/www_root/adm/msglist.php
@@ -2,7 +2,7 @@
 /***************************************************************************
 * copyright            : (C) 2001-2003 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: msglist.php,v 1.22 2003/11/21 20:37:24 hackie Exp $
+* $Id: msglist.php,v 1.23 2003/11/23 12:18:04 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it 
 * under the terms of the GNU General Public License as published by the 
@@ -97,7 +97,7 @@ function makedeps()
 			if (($e = strpos($data, "\n", $s)) === false) {
 				continue;
 			}
-			$data = substr_replace($data, $_POST[$v], $s, ($e - $s));
+			$data = substr_replace($data, trim($_POST[$v]), $s, ($e - $s));
 		}
 		if (!($fp = fopen($msgfile, 'wb'))) {
 			exit('unable to write to "'.$msgfile.'" message file');
