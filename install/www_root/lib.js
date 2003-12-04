7a13052f327f6f5b41a2677ba958aaf59eefac04
iliaa <ilia@ilia.ws>
2003-12-04 13:25:26 +0000

    Optimize png displaying.

--- install/www_root/lib.js
+++ install/www_root/lib.js
@@ -207,19 +207,19 @@ function fud_msg_focus(mid_hash)
 // The code comes from youngpup.net. Thanks youngpup!
 
 if (navigator.platform == "Win32" && navigator.appName == "Microsoft Internet Explorer" && window.attachEvent) {
-	document.writeln('<style type="text/css">img { visibility:hidden; } </style>');
-	window.attachEvent("onload", fnLoadPngs);
+	rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
+	itsAllGood = (rslt != null && Number(rslt[1]) >= 5.5);
+
+	if (itsAllGood) {
+		document.writeln('<style type="text/css">img { visibility:hidden; } </style>');
+		window.attachEvent("onload", fnLoadPngs);
+	}
 }
 
 function fnLoadPngs() {
-	var rslt = navigator.appVersion.match(/MSIE (\d+\.\d+)/, '');
-	var itsAllGood = (rslt != null && Number(rslt[1]) >= 5.5);
-
 	for (var i = document.images.length - 1, img = null; (img = document.images[i]); i--) {
-		if (itsAllGood && img.src.match(/\.png$/i) != null) {
+		if (img.src.match(/\.png$/i) != null) {
 			var src = img.src;
-			img.style.width = img.width + "px";
-			img.style.height = img.height + "px";
 			img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + src + "', sizingMethod='scale')"
 			img.src = "blank.gif";
 		}
