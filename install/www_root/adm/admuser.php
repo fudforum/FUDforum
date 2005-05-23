1dcfe302054235ce945e78fffc5f66ec6e62dbbc
iliaa <ilia@ilia.ws>
2005-05-23 23:22:37 +0000

    Add missing escape.

--- install/www_root/adm/admuser.php
+++ install/www_root/adm/admuser.php
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2004 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admuser.php,v 1.57 2005/04/01 20:47:58 hackie Exp $
+* $Id: admuser.php,v 1.58 2005/05/23 23:22:37 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -186,7 +186,7 @@ administration permissions to the forum. This individual will be able to do anyt
 			q("UPDATE ".$DBHOST_TBL_PREFIX."users SET passwd='".md5($_POST['login_passwd'])."' WHERE id=".$usr_id);
 			logaction(_uid, 'ADM_SET_PASSWD', 0, addslashes(char_fix(htmlspecialchars($u->login))));
 		} else if (!empty($_POST['login_name']) && $u->login != $_POST['login_name']) { /* chanding login name */
-			$alias = "'" . make_alias($_POST['login_name']) . "'";
+			$alias = "'" . addslashes(make_alias($_POST['login_name'])) . "'";
 			$login = "'" . addslashes($_POST['login_name']) . "'";
 
 			if ($FUD_OPT_2 & 128) {
