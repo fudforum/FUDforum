5fc1444ea8fa96834c39c3eac3e2f05452771f5a
iliaa <ilia@ilia.ws>
2005-10-11 23:24:57 +0000

    Don't use base64 encoded error log format.

--- install/forum_data/src/err.inc.t
+++ install/forum_data/src/err.inc.t
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2004 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: err.inc.t,v 1.48 2005/07/18 19:08:02 hackie Exp $
+* $Id: err.inc.t,v 1.49 2005/10/11 23:24:57 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -28,7 +28,8 @@ function error_dialog($title, $msg, $level='WARN', $ses=null)
 	if (isset($_SERVER['HTTP_REFERER'])) {
 		$error_msg .= '[Referring Page] '.htmlspecialchars($_SERVER['HTTP_REFERER']).'<br />';
 	}
-	error_log('['.gmdate('D M j G:i:s T Y', __request_timestamp__).'] '.base64_encode($error_msg)."\n", 3, $GLOBALS['ERROR_PATH'].'fud_errors');
+	$pfx = sprintf("?%-10d?%-10d?", strlen($error_msg) + 1, __request_timestamp__);
+	error_log($pfx.$error_msg."\n", 3, $GLOBALS['ERROR_PATH'].'fud_errors');
 
 	/* no need to redirect, we just want to log the error */
 	if ($level == 'ATCH') {
--- install/www_root/adm/admerr.php
+++ install/www_root/adm/admerr.php
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2004 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admerr.php,v 1.19 2005/09/27 22:39:14 hackie Exp $
+* $Id: admerr.php,v 1.20 2005/10/11 23:24:57 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -26,31 +26,34 @@
 <?php
 	$err = 0;
 
+function print_log($path)
+{
+	echo '<table class="resulttable"><tr class="resulttopic"><td>Time</td><td>Error Description</td></tr>';
+	$fp = fopen($path, 'r');
+	while (1) {
+		// ?%-10d?%-10d?
+		if (!($pfx = fread($fp, 23))) {
+			break;
+		}
+		if ($pfx{0} != '?') { // for old log entries
+			continue;
+		}
+		list(,$s,$d,) = explode('?', $pfx);
+		echo '<tr class="field"><td nowrap valign="top">'.gmdate('D M j G:i:s T Y', $d).'</td><td>'.fread($fp, $s).'</td></tr>';
+	}
+	fclose($fp);
+	echo '</table><br /><br />';
+}
+
 	if (@file_exists($ERROR_PATH.'fud_errors') && filesize($ERROR_PATH.'fud_errors')) {
 		echo '<h4>FUDforum Error Log [<a href="admerr.php?clear_fud_log=1&'.__adm_rsidl.'">clear log</a>]</h4>';
-		echo '<table class="resulttable"><tr class="resulttopic"><td>Time</td><td>Error Description</td></tr>';
-
-		$fp = fopen($ERROR_PATH.'fud_errors', "r");
-		while (($error = fgets($fp))) {
-			list($time, $msg) = explode('] ', substr($error, 1));
-			echo '<tr class="field"><td nowrap valign="top">'.$time.'</td><td>'.base64_decode($msg).'</td></tr>';
-		}
-		fclose($fp);
-		echo '</table><br /><br />';
+		print_log($ERROR_PATH.'fud_errors');
 		$err = 1;
 	}
 
 	if (@file_exists($ERROR_PATH.'sql_errors') && filesize($ERROR_PATH.'sql_errors')) {
 		echo '<h4>SQL Error Log [<a href="admerr.php?clear_sql_log=1&'.__adm_rsidl.'">clear log</a>]</h4>';
-		echo '<table border=1 cellspacing=1 cellpadding=3><tr bgcolor="#bff8ff"><td>Time</td><td>Error Description</td></tr>';
-
-		$fp = fopen($ERROR_PATH.'sql_errors', "r");
-		while (($error = fgets($fp))) {
-			list($time, $msg) = explode('] ', substr($error, 1));
-			echo '<tr class="field"><td nowrap valign="top">'.$time.'</td><td>'.base64_decode($msg).'</td></tr>';
-		}
-		fclose($fp);
-		echo '</table><br /><br />';
+		print_log($ERROR_PATH.'sql_errors');
 		$err = 1;
 	}
 
