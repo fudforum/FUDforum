59f802f69b9c9f265179a5bbf134071465d44e90
iliaa <ilia@ilia.ws>
2005-10-11 15:27:50 +0000

    Allow mass-email control panel to send messages via PM.

--- install/www_root/adm/admmassemail.php
+++ install/www_root/adm/admmassemail.php
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2004 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admmassemail.php,v 1.37 2005/10/11 14:37:28 hackie Exp $
+* $Id: admmassemail.php,v 1.38 2005/10/11 15:27:50 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -38,67 +38,77 @@
 			$c = uq('SELECT id FROM '.$DBHOST_TBL_PREFIX.'users WHERE id=-1');
 		} else {
 			$gid = (int) $_POST['group'];
+			$fld = !empty($_POST['pm']) ? $DBHOST_TBL_PREFIX.'users.id' : 'email';
+
 			if ($gid > 0) {
-				$c = uq('SELECT email FROM '.$DBHOST_TBL_PREFIX.'group_members gm INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=gm.user_id WHERE gm.group_id='.$gid.(isset($_POST['ignore_override']) ? '' : ' AND (users_opt & 8)=0'));
+				$c = uq('SELECT'.$fld.'FROM '.$DBHOST_TBL_PREFIX.'group_members gm INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=gm.user_id WHERE gm.group_id='.$gid.(isset($_POST['ignore_override']) ? '' : ' AND (users_opt & 8)=0'));
 			} else if ($gid == $all_mods) {
-				$c = uq('SELECT email FROM '.$DBHOST_TBL_PREFIX.'mod m INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=m.user_id '.(isset($_POST['ignore_override']) ? '' : ' WHERE (users_opt & 8)=0'));
+				$c = uq('SELECT'.$fld.'FROM '.$DBHOST_TBL_PREFIX.'mod m INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=m.user_id '.(isset($_POST['ignore_override']) ? '' : ' WHERE (users_opt & 8)=0'));
 			} else if ($gid == $all_grp_lead) {
-				$c = uq('SELECT email FROM '.$DBHOST_TBL_PREFIX.'group_members gm INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=gm.user_id WHERE (gm.group_members_opt & 131072) '.(isset($_POST['ignore_override']) ? '' : ' AND (users_opt & 8)=0'));
+				$c = uq('SELECT'.$fld.'FROM '.$DBHOST_TBL_PREFIX.'group_members gm INNER JOIN '.$DBHOST_TBL_PREFIX.'users u ON u.id=gm.user_id WHERE (gm.group_members_opt & 131072) '.(isset($_POST['ignore_override']) ? '' : ' AND (users_opt & 8)=0'));
 			} else {
-				$c = uq('SELECT email FROM '.$DBHOST_TBL_PREFIX.'users WHERE level_id='.($gid * -1).(isset($_POST['ignore_override']) ? '' : ' AND id > 1 AND (users_opt & 8)=0'));
+				$c = uq('SELECT'.$fld.'FROM '.$DBHOST_TBL_PREFIX.'users WHERE level_id='.($gid * -1).(isset($_POST['ignore_override']) ? '' : ' AND id > 1 AND (users_opt & 8)=0'));
 			}
 		}
 
-		$email_block = 50;
-		$total = $email_block_stat = 0;
-		$send_to = $ADMIN_EMAIL;
-		$to = array();
-
-		if (!($FUD_OPT_1 & 512)) {
-			if (version_compare("4.3.3RC2", PHP_VERSION, ">")) {
-				$_POST['body'] = str_replace("\n.", "\n..", $_POST['body']);
-			}
-
+		if (!empty($_POST['pm'])) {
+			fud_use('ssu.inc');
+			$to = array();
 			while ($r = db_rowarr($c)) {
-				$to[] = $r[0];
-				if (!(++$email_block_stat % $email_block)) {
-					$email_block_stat = 0;
+				$to[] = (int)$r[0];
+			}
+			send_status_update($to, '', '', $_POST['subject'], $_POST['body']);
+		} else {
+			$email_block = 50;
+			$total = $email_block_stat = 0;
+			$send_to = $ADMIN_EMAIL;
+			$to = array();
+
+			if (!($FUD_OPT_1 & 512)) {
+				if (version_compare("4.3.3RC2", PHP_VERSION, ">")) {
+					$_POST['body'] = str_replace("\n.", "\n..", $_POST['body']);
+				}
+
+				while ($r = db_rowarr($c)) {
+					$to[] = $r[0];
+					if (!(++$email_block_stat % $email_block)) {
+						$email_block_stat = 0;
+						$bcc = implode(', ', $to) . "\r\n";
+						mail(' ', encode_subject($_POST['subject']), $_POST['body'], "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
+						$to = array();
+					}
+					++$total;
+				}
+				unset($c);
+				if ($to) {
 					$bcc = implode(', ', $to) . "\r\n";
 					mail(' ', encode_subject($_POST['subject']), $_POST['body'], "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
-					$to = array();
 				}
-				++$total;
-			}
-			unset($c);
-			if ($to) {
-				$bcc = implode(', ', $to) . "\r\n";
-				mail(' ', encode_subject($_POST['subject']), $_POST['body'], "From: ".$ADMIN_EMAIL."\r\nReply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\nX-Mailer: FUDforum v".$GLOBALS['FORUM_VERSION']."\r\nBcc: ".$bcc);
-			}
-		} else {
-			$smtp = new fud_smtp;
-			$smtp->msg = str_replace("\n.", "\n..", $_POST['body']);
-			$smtp->subject = encode_subject($_POST['subject']);
-			$smtp->from = $ADMIN_EMAIL;
-			$smtp->headers = "Reply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\n";
+			} else {
+				$smtp = new fud_smtp;
+				$smtp->msg = str_replace("\n.", "\n..", $_POST['body']);
+				$smtp->subject = encode_subject($_POST['subject']);
+				$smtp->from = $ADMIN_EMAIL;
+				$smtp->headers = "Reply-To: ".$ADMIN_EMAIL."\r\nErrors-To: ".$ADMIN_EMAIL."\r\n";
 
-			while ($r = db_rowarr($c)) {
-				$to[] = $r[0];
-				if (!(++$email_block_stat % $email_block)) {
-					$email_block_stat = 0;
+				while ($r = db_rowarr($c)) {
+					$to[] = $r[0];
+					if (!(++$email_block_stat % $email_block)) {
+						$email_block_stat = 0;
 
+						$smtp->to = $to;
+						$smtp->send_smtp_email();
+
+						$to = array();
+					}
+					++$total;
+				}
+				if (count($to)) {
 					$smtp->to = $to;
 					$smtp->send_smtp_email();
-
-					$to = array();
 				}
-				++$total;
-			}
-			if (count($to)) {
-				$smtp->to = $to;
-				$smtp->send_smtp_email();
 			}
 		}
-
 		if (!$err) {
 			echo '<font size="+1" color="green">'.$total.' E-mails were sent</font><br />';
 		}
@@ -110,8 +120,14 @@
 <form method="post" name="a_frm">
 <?php echo _hs; ?>
 <table class="datatable solidtable">
+<?php if($FUD_OPT_1 & 1024) { ?>
+<tr class="field">
+	<td>Send Messages via PM</td>
+	<td><input type="checkbox" name="pm" value="1"></td>
+</tr>
 <?php
-	echo '<tr class="field"><td valign=top>Send E-mails To</td><td><select name="group">';
+}
+	echo '<tr class="field"><td valign=top>Send Messages To</td><td><select name="group">';
 	echo '<option value="0">All Forum Members</option>';
 	echo '<option value="'.$all_mods.'">All Forum Moderators</option>';
 	echo '<option value="'.$all_grp_lead.'">All Group Leaders</option>';
