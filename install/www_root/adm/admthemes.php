ed84ca3ecf24eb3a8353a172d07d41085e3ccbf1
iliaa <ilia@ilia.ws>
2005-11-29 16:51:45 +0000

    Added safety check to the theme manager to ensure that PATH_INFO is enabled before allowing creation of themes based on PATH_INFO urls.

--- install/www_root/adm/admthemes.php
+++ install/www_root/adm/admthemes.php
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2004 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admthemes.php,v 1.59 2005/09/08 14:17:13 hackie Exp $
+* $Id: admthemes.php,v 1.60 2005/11/29 16:51:45 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -48,6 +48,11 @@
 			compile_all($data[0], $data[1], $data[2]);
 		}
 		unset($r);
+	}
+
+	if (isset($_POST['thm_theme']) && $_POST['thm_theme'] == 'path_info' && !($FUD_OPT_2 & 32768)) {
+		unset($_POST['edit'], $_POST['thm_theme']);
+		echo '<h1 class="alert">You need to enable PATH_INFO support in forum settings before using path_info theme.</h2>';
 	}
 
 	if (isset($_POST['thm_theme']) && !$edit) {
