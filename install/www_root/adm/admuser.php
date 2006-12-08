b6f1eed5c2b671212c4778b87aa9924e02626fc8
iliaa <ilia@ilia.ws>
2006-12-08 17:25:21 +0000

    Show last used IP

--- install/www_root/adm/admuser.php
+++ install/www_root/adm/admuser.php
@@ -2,7 +2,7 @@
 /**
 * copyright            : (C) 2001-2006 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: admuser.php,v 1.76 2006/11/28 16:03:11 hackie Exp $
+* $Id: admuser.php,v 1.77 2006/12/08 17:25:21 hackie Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
@@ -332,6 +332,9 @@ document.frm_usr.usr_login.focus();
 	}
 	if ($u->reg_ip) {
 		echo '<tr class="field"><td>Registration IP:</td><td>' . long2ip($u->reg_ip) . '</td></tr>';
+	}
+	if ($u->last_known_ip) {
+		echo '<tr class="field"><td>Last Used IP:</td><td>' . long2ip($u->last_known_ip) . '</td></tr>';
 	}
 
 	echo '<tr class="field"><td align=middle colspan=2><font size="+1">&gt;&gt; <a href="../'.__fud_index_name__.'?t=register&mod_id='.$usr_id.'&'.__adm_rsidl.'">Change User\'s Profile</a> &lt;&lt;</font></td></tr>';
