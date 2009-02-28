c60d7ae2a087e2f144f12e8488be60861322e4f3
naudefj <naudefj@gmail.com>
2009-02-28 11:47:18 +0000

    Fix syntax error (typo)

--- install/forum_data/scripts/maillist.php
+++ install/forum_data/scripts/maillist.php
@@ -3,7 +3,7 @@
 /**
 * copyright            : (C) 2001-2009 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: maillist.php,v 1.76 2009/01/29 18:37:17 frank Exp $
+* $Id: maillist.php,v 1.77 2009/02/28 11:47:18 frank Exp $
 *
 * This program is free software; you can redistribute it and/or modify it 
 * under the terms of the GNU General Public License as published by the 
@@ -482,7 +482,7 @@ function add_attachment($name, $data, $pid)
 	$msg_post->subject = htmlspecialchars(apply_custom_replace($emsg->subject));
 	if (!strlen($msg_post->subject)) {
 		mlist_error_log("Blank Subject", $emsg->raw_msg);
-		msg_post->subject = "(no subject)";
+		$msg_post->subject = "(no subject)";
 	}
 
 	$msg_post->ip_addr = $emsg->ip;
