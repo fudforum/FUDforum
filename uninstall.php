786144007ddc5380c2abe03104e698d91acf1505
naudefj <naudefj@gmail.com>
2009-07-19 19:29:40 +0000

    More uninstall checks & fix drop tables

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,10 @@
 <?php
 exit("To run the un-installer, comment out the 2nd line of the script!\n");
+
 /***************************************************************************
 * copyright            : (C) 2001-2009 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: uninstall.php,v 1.23 2009/02/04 10:44:49 frank Exp $
+* $Id: uninstall.php,v 1.24 2009/07/19 19:29:40 frank Exp $
 *
 * This program is free software; you can redistribute it and/or modify it 
 * under the terms of the GNU General Public License as published by the 
@@ -35,14 +36,15 @@ function fud_ini_get($opt)
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
-	<title>FUDforum Un-installer</title>
+	<title>FUDforum Uninstaller</title>
 </head>
 <body>
 <table style="background: #527bbd; color: white; width: 100%; height: 50px;"><tr>
-  <td> &nbsp; </td>
+  <td><img src="images/fudlogo.gif" alt="" style="float:left;" border="0" /></td>
   <td><span style="color: #fff; font-weight: bold; font-size: x-large;">FUDforum Uninstall Wizard</span></td>
   <td> &nbsp; </td>
 </tr></table>
+<br />
 
 <?php
 
@@ -78,32 +80,36 @@ function fud_rmdir($dir)
 }
 
 	if (isset($SERVER_DATA_ROOT)) {
+		/* sanity checks */
 		if (!is_dir($SERVER_DATA_ROOT)) {
-			print_error('Directory "'.$SERVER_DATA_ROOT.'" does not exist!');
+			print_error('Forum Data Root directory "'.$SERVER_DATA_ROOT.'" does not exist!');
 		}
+		if (!empty($SERVER_ROOT) && !is_dir($SERVER_ROOT)) {
+			print_error('Server Root directory "'.$SERVER_ROOT.'" does not exist!');
+		}
+		if (!file_exists($SERVER_DATA_ROOT . '/include/GLOBALS.php')) {
+			print_error('Directory "'.$SERVER_DATA_ROOT.'" does not appear to be a Forum Data Root directory!');
+		}
+		if (!empty($SERVER_ROOT) && !file_exists($SERVER_ROOT . '/adm/admpanel.php')) {
+			print_error('Directory "'.$SERVER_ROOT.'" does not appear to be a Server Root directory!');
+		}		
 
-		/* try to read GLOBALS.php to determine database settings so that databases can be cleaned up */
-		if (file_exists($SERVER_DATA_ROOT . 'include/GLOBALS.php')) {
-			$data = file_get_contents($SERVER_DATA_ROOT . 'include/GLOBALS.php');
-			$s = strpos($data, '*/') + 2;
-			$data = substr($data, $s, (strpos($data, 'DO NOT EDIT FILE BEYOND THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING', $s) - $s)) . ' */';
-			eval($data);
-			
-			if (!empty($DBHOST) && file_exists($SERVER_DATA_ROOT . 'include/theme/default/db.inc')) {
-				$data = file_get_contents($SERVER_DATA_ROOT . 'include/GLOBALS.php');
-				if (strpos($data, "define('__dbtype__', 'mysql')") !== FALSE) {
-					$dbtype = 'mysql';
-				} else {
-					$dbtype = 'pgsql';
-				}
-				$remove_db = 1;
+		/* read GLOBALS.php to determine database settings so that databases can be cleaned up */
+		$data = file_get_contents($SERVER_DATA_ROOT . '/include/GLOBALS.php');
+		$s = strpos($data, '*/') + 2;
+		$data = substr($data, $s, (strpos($data, 'DO NOT EDIT FILE BEYOND THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING', $s) - $s)) . ' */';
+		eval($data);
+
+		if (file_exists($SERVER_DATA_ROOT . '/include/theme/default/db.inc')) {
+			$data = file_get_contents($SERVER_DATA_ROOT . '/include/theme/default/db.inc');
+			if (strpos($data, "define('__dbtype__', 'mysql')") !== FALSE) {
+				$dbtype = 'mysql';
+			} else {
+				$dbtype = 'pgsql';
 			}
-		}
-
-		/* remove files on disk */
-		fud_rmdir($SERVER_DATA_ROOT);
-		if ($SERVER_ROOT != $SERVER_DATA_ROOT && $SERVER_ROOT) {
-			fud_rmdir($SERVER_ROOT);
+			$remove_db = 1;
+		} else {
+			echo 'Unable to detect database type, tables will not be dropped.<br />';
 		}
 
 		/* remove database stuff if needed */
@@ -113,6 +119,7 @@ function fud_rmdir($dir)
 					if (@mysql_select_db($DBHOST_DBNAME) && $DBHOST_TBL_PREFIX) {
 						$c = mysql_query("SHOW TABLES LIKE '".$DBHOST_TBL_PREFIX."%'");
 						while ($r = mysql_fetch_row($c)) {
+							echo 'Dropping table '.$r[0].'<br />';
 							mysql_query('DROP TABLE '.$r[0]);
 						}
 					}
@@ -133,19 +140,34 @@ function fud_rmdir($dir)
 				}
 				if (($conn = @pg_connect($connect_str))) {
 					while ($r = pg_fetch_row($c)) {
+						echo 'Dropping table '.$r[0].'<br />';
 						pg_query($conn, 'DROP TABLE '.$r[0]);
 					}
 				}
 			}
 		}
-		print_error('FUDforum installed in "'.$SERVER_DATA_ROOT.'" has been successfully uninstalled!<br /><br />Sorry to see you go. If there is anything we can do to help, please let us know at <a href="http://fudforum.org/">fudforum.org</a>.');
+
+		/* remove symlinks first - unlink doesn't delete broken symlinks */
+		@unlink($SERVER_DATA_ROOT . '/scripts/GLOBALS.php');
+		@unlink((empty($SERVER_ROOT) ? $SERVER_DATA_ROOT : $SERVER_DATA_ROOT) . '/GLOBALS.php');
+		@unlink((empty($SERVER_ROOT) ? $SERVER_DATA_ROOT : $SERVER_DATA_ROOT) . '/adm/GLOBALS.php');
+		
+		/* remove files on disk */
+		echo 'Removing files in directory '.$SERVER_DATA_ROOT.'<br />';
+		fud_rmdir($SERVER_DATA_ROOT);
+		if ($SERVER_ROOT != $SERVER_DATA_ROOT && $SERVER_ROOT) {
+			echo 'Removing files in directory '.$SERVER_ROOT.'<br />';
+			fud_rmdir($SERVER_ROOT);
+		}
+
+		print_error('FUDforum was successfully uninstalled!<br /><br />Sorry to see you go. If there is anything we can do to help, please let us know on the support forum at <a href="http://fudforum.org/">fudforum.org</a>.');
 	}
 ?>
 <div align="center">
 <form name="uninstall" action="uninstall.php" method="post">
 <table bgcolor="#000000" align="center" border="0" cellspacing="0" cellpadding="1">
 <tr><td><table bgcolor="#FFFFFF" border="0" cellspacing="1" cellpadding="4" align="center">
-	<tr><td colspan="2" bgcolor="#e5ffe7"><font color="red"><b>This utility will uninstall FUDforum from specified directories. Make sure that this is what you want to do, because once it runs there is no going back. We recommend running a backup first.</b></font></td></tr>
+	<tr><td colspan="2" bgcolor="#e5ffe7"><font color="red"><b>This utility will uninstall FUDforum from the specified directories. Make sure that this is what you want to do, because once it runs there is no going back. We recommend running a full backup of your system before continuing.</b></font></td></tr>
 	<tr bgcolor="#bff8ff"><td valign="top"><b>Forum Data Root</b><br /><font size="-1">This is the directory where you've installed the non-browseable forum files</font></td><td><input type="text" name="SERVER_DATA_ROOT" value="" size=40 /></td></tr>
 	<tr bgcolor="#bff8ff"><td valign="top"><b>Server Root</b><br /><font size="-1">This is the directory where you've installed the browseable forum files. If it is the same as "Forum Data Root", you can leave this field blank.</font></td><td><input type="text" name="SERVER_ROOT" value="" size="40" /></td></tr>
 	<tr><td colspan="2" align="center" bgcolor="white"><input type="submit" name="submit" value="uninstall" style="background:red; color:white; font-size: large;" /></td></tr>
