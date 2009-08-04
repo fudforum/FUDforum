307b619082fe075b3c8694f9ec9a35b3249a73f0
naudefj <naudefj@gmail.com>
2009-08-04 19:26:58 +0000

    Change double to single quotes (slightly faster)

--- install/forum_data/include/GLOBALS.php
+++ install/forum_data/include/GLOBALS.php
@@ -2,51 +2,51 @@
 /**
 * copyright            : (C) 2001-2009 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: GLOBALS.php,v 1.63 2009/07/19 20:09:10 frank Exp $
+* $Id: GLOBALS.php,v 1.64 2009/08/04 19:26:57 frank Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
 * Free Software Foundation; version 2 of the License.
 **/
 
-	$INCLUDE 		= "";
-	$WWW_ROOT 		= "";
-	$WWW_ROOT_DISK		= "";
-	$DATA_DIR		= "";
-	$ERROR_PATH 		= "";
-	$MSG_STORE_DIR		= "";
-	$TMP			= "";
-	$FILE_STORE		= "";
-	$FORUM_SETTINGS_PATH 	= "";
-	$PLUGIN_PATH 		= "";
+	$INCLUDE 		= '';
+	$WWW_ROOT 		= '';
+	$WWW_ROOT_DISK		= '';
+	$DATA_DIR		= '';
+	$ERROR_PATH 		= '';
+	$MSG_STORE_DIR		= '';
+	$TMP			= '';
+	$FILE_STORE		= '';
+	$FORUM_SETTINGS_PATH 	= '';
+	$PLUGIN_PATH 		= '';
 
 	$FUD_OPT_1		= 1743713471;
 	$FUD_OPT_2		= 695668799;
 	$FUD_OPT_3		= 0;
 
 	$CUSTOM_AVATAR_MAX_SIZE = 10000;	/* bytes */
-	$CUSTOM_AVATAR_MAX_DIM	= "64x64";	/* width x height (pixels) */
+	$CUSTOM_AVATAR_MAX_DIM	= '64x64';	/* width x height (pixels) */
 
-	$COOKIE_PATH		= "";
-	$COOKIE_DOMAIN		= "";
-	$COOKIE_NAME		= "";
+	$COOKIE_PATH		= '';
+	$COOKIE_DOMAIN		= '';
+	$COOKIE_NAME		= '';
 	$COOKIE_TIMEOUT 	= 604800;	/* seconds */
 	$SESSION_TIMEOUT 	= 1800;		/* seconds */
 
-	$DBHOST 		= "";
-	$DBHOST_USER		= "";
-	$DBHOST_PASSWORD	= "";
-	$DBHOST_DBNAME		= "";
-	$DBHOST_TBL_PREFIX	= "fud28_";		/* do not modify this */
-	$DBHOST_DBTYPE		= "";
+	$DBHOST 		= '';
+	$DBHOST_USER		= '';
+	$DBHOST_PASSWORD	= '';
+	$DBHOST_DBNAME		= '';
+	$DBHOST_TBL_PREFIX	= 'fud28_';		/* do not modify this */
+	$DBHOST_DBTYPE		= '';
 
-	$FUD_SMTP_SERVER	= "";
+	$FUD_SMTP_SERVER	= '';
 	$FUD_SMTP_PORT		= 25;
 	$FUD_SMTP_TIMEOUT	= 10;		/* seconds */
-	$FUD_SMTP_LOGIN		= "";
-	$FUD_SMTP_PASS		= "";
+	$FUD_SMTP_LOGIN		= '';
+	$FUD_SMTP_PASS		= '';
 
-	$ADMIN_EMAIL 		= "";
+	$ADMIN_EMAIL 		= '';
 
 	$PRIVATE_ATTACHMENTS	= 5;		/* int */
 	$PRIVATE_ATTACH_SIZE	= 1000000;	/* bytes */
@@ -61,15 +61,15 @@
 	$MOVED_THR_PTR_EXPIRY	= 3;		/* days */
 
 	$MAX_SMILIES_SHOWN	= 15;		/* int */
-	$DISABLED_REASON	= "Temporarily offline; please come back soon!";
+	$DISABLED_REASON	= 'Temporarily offline; please come back soon!';
 	$POSTS_PER_PAGE 	= 40;
 	$THREADS_PER_PAGE	= 40;
 	$WORD_WRAP		= 60;
-	$NOTIFY_FROM		= "";		/* email */
-	$ANON_NICK		= "Anonymous Coward";
+	$NOTIFY_FROM		= '';		/* email */
+	$ANON_NICK		= 'Anonymous Coward';
 	$FLOOD_CHECK_TIME	= 60;		/* seconds */
 	$POSTS_BEFORE_LINKS	= 0;
-	$SERVER_TZ		= "America/Montreal"; /* timezone code from tz.inc */
+	$SERVER_TZ		= 'America/Montreal'; /* timezone code from tz.inc */
 	$SEARCH_CACHE_EXPIRY	= 172800;	/* seconds */
 	$MEMBERS_PER_PAGE	= 40;
 	$POLLS_PER_PAGE		= 40;
@@ -79,8 +79,8 @@
 	$LOGEDIN_TIMEOUT	= 5;		/* minutes */
 	$MAX_IMAGE_COUNT	= 10;
 	$STATS_CACHE_AGE	= 600;		/* seconds */
-	$FORUM_TITLE		= "My forum, my way!";
-	$FORUM_DESCR		= "Fast Uncompromising Discussions. Get your users talking!";
+	$FORUM_TITLE		= 'My forum, my way!';
+	$FORUM_DESCR		= 'Fast Uncompromising Discussions. FUDforum will get your users talking!';
 	$MAX_LOGIN_SHOW		= 25;
 	$MAX_LOCATION_SHOW	= 25;
 	$SHOW_N_MODS		= 2;
@@ -99,12 +99,12 @@
 	$FEED_AUTH_ID		= 0;		/* 0 - treat as anon user, >0 treat like specific forum user */
 	$FEED_CACHE_AGE		= 900;
 
-	$PDF_PAGE		= "letter";	/* string */
+	$PDF_PAGE		= 'letter';	/* string */
 	$PDF_WMARGIN		= 15;		/* int */
 	$PDF_HMARGIN		= 15;		/* int */
 	$PDF_MAX_CPU		= 60;		/* seconds */
 
-	$FUD_WHOIS_SERVER	= "ws.arin.net";
+	$FUD_WHOIS_SERVER	= 'ws.arin.net';
 	$MIN_TIME_BETWEEN_LOGIN = 10;		/* seconds */
 
 /* DO NOT EDIT FILE BEYOND THIS POINT UNLESS YOU KNOW WHAT YOU ARE DOING */
