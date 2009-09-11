b85290860d8d883892ae3dbbd7283b817fb7da2d
naudefj <naudefj@gmail.com>
2009-09-11 17:36:12 +0000

    Add missing locale files

--- install/forum_data/thm/default/i18n/tr_download.php
+++ install/forum_data/thm/default/i18n/tr_download.php
@@ -3,13 +3,14 @@
 /**
 * copyright            : (C) 2001-2009 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
-* $Id: tr_download.php,v 1.1 2009/09/10 15:19:28 frank Exp $
+* $Id: tr_download.php,v 1.2 2009/09/11 17:36:12 frank Exp $
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
 * Free Software Foundation; version 2 of the License.
 **/
 
+	$i = 0;
 	$dp = opendir('.');
 	while ($de = readdir($dp)) {
 		if (!is_dir($de) || $de == '.' || $de == '..' || $de == 'english' || !@file_exists($de . '/pspell_lang')) {
@@ -51,6 +52,8 @@
 		}
 
 		sleep(5);
+		$i++;
 	}
 	closedir($dp);
+	echo "\nTotal translations: $i\n";
 ?>
