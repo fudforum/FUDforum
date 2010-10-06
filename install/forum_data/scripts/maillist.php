1a34f7b9831bb649d97dbf8479ad6e45bd3990d5
naudefj <naudefj@gmail.com>
2010-10-06 19:52:19 +0000

    Abstract the MIME code from the Mailing List Manager into a separate class that can also be used for Usenet posts.

--- install/forum_data/scripts/maillist.php
+++ install/forum_data/scripts/maillist.php
@@ -10,315 +10,25 @@
 * Free Software Foundation; version 2 of the License. 
 **/
 
-class fud_emsg
+function log_script_error($error, $msg_data='', $level='WARNING')
 {
-	var $subject, $body, $headers, $to, $from_email, $from_name, $ip, $msg_id;
-	var $subject_cleanup_rgx, $body_cleanup_rgx, $subject_cleanup_rep, $body_cleanup_rep;
-	var $user_id;
-	var $reply_to_msg_id;
-	var $reply_to, $thread_id;
-	var $raw_msg;
-	var $body_s, $body_sc;
-	var $attachments  = array(), $inline_files = array();
-
-	function read_data($data='')
-	{
-		$this->raw_msg = !$data ? file_get_contents('php://stdin') : $data;
-	}
-
-	function split_hdr_body()
-	{
-		if (!preg_match("!^(.*?)\r?\n\r?\n(.*)!s", $this->raw_msg, $m)) {
-			return;
-		}
-
-		$this->body = $m[2];
-		$this->headers = $m[1];
-	}
-
-	function format_header()
-	{
-		$this->headers = str_replace("\r\n", "\n", $this->headers);
-		// Cleanup multiline headers.
-		$this->headers = preg_replace("!\n(\t| )+!", ' ', $this->headers);
-		$hdr = explode("\n", trim($this->headers));
-		$this->headers = array();
-		foreach ($hdr as $v) {
-			$hk = substr($v, 0, ($p = strpos($v, ':')));
-			// Skip non-valid header lines.
-			if (!$hk || ++$p == strlen($v) || ($v{$p} != ' ' && $v{$p} != "\t")) {
-				continue;
-			}
-
-			$hv = substr($v, $p);
-			$hk = strtolower(trim($hk));
-
-			if (!isset($this->headers[$hk])) {
-				$this->headers[$hk] = decode_header_value($hv);
-			} else {
-				$this->headers[$hk] .= ' '. decode_header_value($hv);
-			}
-		}
-	}
-
-	function parse_multival_headers($val, $key)
-	{
-		if (($p = strpos($val, ';')) !== false) {
-			$this->headers[$key] = strtolower(trim(substr($val, 0, $p)));
-			$val = ltrim(substr($val, $p+1));
-			if (!empty($val) && preg_match_all('!([-A-Za-z]+)="?(.*?)"?\s*(?:;|$)!', $val, $m)) {
-				$c = count($m[0]);
-				for ($i=0; $i<$c; $i++) {
-					$this->headers['__other_hdr__'][$key][strtolower($m[1][$i])] = $m[2][$i];
-				}
-			}
-		} else {
-			$this->headers[$key] = strtolower(trim($val));
-		}
-	}
-
-	function handle_content_headers()
-	{
-		// This functions performs special handling needed for parsing message data.
-
-		if (isset($this->headers['content-type'])) {
-			$this->parse_multival_headers($this->headers['content-type'], 'content-type');
-		} else {
-			$this->headers['content-type'] = 'text/plain';
-			$this->headers['__other_hdr__']['content-type']['charset'] = 'utf-8';
-		}
-
-		if (isset($this->headers['content-disposition'])) {
-			$this->parse_multival_headers($this->headers['content-disposition'], 'content-disposition');
-		} else {
-			$this->headers['content-disposition'] = 'inline';
-		}
-		if (isset($this->headers['content-transfer-encoding'])) {
-			$this->parse_multival_headers($this->headers['content-transfer-encoding'], 'content-transfer-encoding');
-		} else {
-			$this->headers['content-transfer-encoding'] = '7bit';
-		}
-	}
-
-	function boudry_split($boundry, $html)
-	{
-		// Isolate boundry sections.
-		$this->body_sc = 0;
-		foreach (explode('--'. $boundry, $this->body) as $p) {
-			if (!trim($p)) continue;
-			// Parse inidividual body sections.
-			$this->body_s[$this->body_sc] = new fud_emsg;
-			$this->body_s[$this->body_sc++]->parse_input($html, $p);
-		}
-	}
-
-	function decode_body($html=0)
-	{
-		switch ($this->headers['content-type']) {
-			case 'text/plain':
-				$this->decode_message_body();
-				break;
-
-			case 'text/html':
-				$this->decode_message_body();
-				if (!$html) {
-					$this->body = str_ireplace('<p>', "\n\n", $this->body);
-					$this->body = str_ireplace(array('<br />', '<br>', '</p>'), "\n", $this->body);
-					$this->body = strip_tags($this->body);
-				}
-				break;
-
-			case 'multipart/parallel': // Apparently same as multipart/mixed but order of body parts does not matter
-	                case 'multipart/report': // RFC1892 ( 1st part is human readable, identical to multipart/mixed )
-        	        case 'multipart/signed': // PGP or OpenPGP (appear same) ( 1st part is human readable )
-			case 'multipart/alternative': // various alternate formats of message most common html or text
-			case 'multipart/related': // ignore those, contains urls/links to 'stuff' on the net
- 			case 'multipart/mixed':
- 			case 'message/rfc822': // *scary*
-
-				if (!isset($this->headers['__other_hdr__']['content-type']['boundary'])) {
-					$this->body = '';
-					return;
-				}
-				$this->boudry_split($this->headers['__other_hdr__']['content-type']['boundary'], $html);
-				// In some cases in multi-part messages there will only be 1 body,
-				// in those situations we assing that body and info to the primary message
-				// and hide the fact this was multi-part message.
-				if ($this->body_sc == 1) {
-					$this->body = $this->body_s[0]->body;
-					$this->headers['__other_hdr__'] = $this->body_s[0]->headers['__other_hdr__'];
-				} else if ($this->body_sc > 1) {
-					// We got many bodies to pick from, Yey!. Lets find something we can use,
-					// preference given to 'text/plain' or if not found go for 'text/html'.
-					$final_id = $html_id = array();
-
-					for ($i = 0; $i < $this->body_sc; $i++) {
-						switch ($this->body_s[$i]->headers['content-type']) {
-							case 'text/html':
-								$html_id[] = $i;
-								break;
-
-							case 'text/plain':
-								$final_id[] = $i;
-								break;
-						}
-
-						// Look if message has any attached files.
-						if ($this->body_s[$i]->headers['content-disposition'] == 'attachment' || 
-								$this->body_s[$i]->headers['content-disposition'] == 'inline' ||
-								isset($this->body_s[$i]->headers['content-id'])) {
-							// Determine the file name.
-							if (isset($this->body_s[$i]->headers['__other_hdr__']['content-disposition']['filename'])) {
-								$file_name = $this->body_s[$i]->headers['__other_hdr__']['content-disposition']['filename'];
-							} else if (isset($this->body_s[$i]->headers['__other_hdr__']['content-type']['name'])) {
-								$file_name = $this->body_s[$i]->headers['__other_hdr__']['content-type']['name'];
-							} else { // No name for file, skipping.
-								continue;
-							}
-
-							$this->attachments[$file_name] = $this->body_s[$i]->body;
-							if (isset($this->body_s[$i]->headers['content-id']) && $this->body_s[$i]->headers['content-disposition'] == 'inline') {
-								$this->inline_files[$file_name] = trim($this->body_s[$i]->headers['content-id'], ' <>');
-							}
-						}
-					}
-					if (($html || !$final_id) && $html_id) {
-						$final_id = $html_id;
-					}
-					if ($final_id) {
-						$this->body = '';
-						foreach ($final_id as $fid) {
-							$this->body .= $this->body_s[$fid]->body;
-							foreach ($this->body_s[$fid]->attachments as $k => $v) {
-								$this->attachments[$k] = $v;
-							}
-							foreach ($this->body_s[$fid]->inline_files as $k => $v) {
-								$this->inline_files[$k] = $v;
-							}
-						}
-						if (isset($this->body_s[$final_id[0]]->headers['__other_hdr__'])) {
-							$this->headers['__other_hdr__'] = $this->body_s[$final_id[0]]->headers['__other_hdr__'];
-						}
-						$this->headers['content-type'] = $this->body_s[$final_id[0]]->headers['content-type'];
-					} else {
-						$this->body = '';
-					}
-				} else { // Bad mail client didn't format message properly.
-					$this->body = '';
-				}
-				break;
-
-			default:
-				$this->decode_message_body();
-				break;
-
-			// case 'multipart/digest':  // Can contain many messages, ignore for our purpose.
-		}
-	}
-
-	function decode_message_body()
-	{
-		$this->body = decode_string($this->body, $this->headers['content-transfer-encoding'], $this->headers['__other_hdr__']['content-type']['charset']);
-	}
-
-	function parse_input($html=0, $data='')
-	{
-		$this->read_data($data);
-		$this->split_hdr_body();
-		$this->format_header();
-		$this->handle_content_headers();
-		$this->decode_body($html);
-	}
-
-	function fetch_useful_headers()
-	{
-		$this->subject = $this->headers['subject'];
-
-		// Attempt to Get Poster's IP from fields commonly used to store it.
-		if (isset($this->headers['x-posted-by'])) {
-			$this->ip = parse_ip($this->headers['x-posted-by']);
-		} else if (isset($this->headers['x-originating-ip'])) {
-			$this->ip = parse_ip($this->headers['x-originating-ip']);
-		} else if (isset($this->headers['x-senderip'])) {
-			$this->ip = parse_ip($this->headers['x-senderip']);
-		} else if (isset($this->headers['x-mdremoteip'])) {
-			$this->ip = parse_ip($this->headers['x-mdremoteip']);
-		} else if (isset($this->headers['received'])) {
-			$this->ip = parse_ip($this->headers['received']);
-		}
-
-		// Fetch From email and Possible name.
-		if (preg_match('!(.*?)<(.*?)>!', $this->headers['from'], $matches)) {
-			$this->from_email = trim($matches[2]);
-
-			if (!empty($matches[1])) {
-				$matches[1] = trim($matches[1]);
-				if ($matches[1][0] == '"' && substr($matches[1], -1) == '"') {
-					$this->from_name = substr($matches[1], 1, -1);
-				} else {
-					$this->from_name = $matches[1];
-				}
-			} else {
-				$this->from_name = $this->from_email;
-			}
-
-			if (preg_match('![^A-Za-z0-9\-_ ]!', $this->from_name)) {
-				$this->from_name = substr($this->from_email, 0, strpos($this->from_email, '@'));
-			}
-		} else {
-			$this->from_email = trim($this->headers['from']);
-			$this->from_name = substr($this->from_email, 0, strpos($this->from_email, '@'));
-		}
-
-		if (empty($this->from_email) || empty($this->from_name)) {
-			mlist_error_log('No name or email for '.$this->headers['from'], $this->raw_msg);
-		}
-
-		if (isset($this->headers['message-id'])) {
-			$this->msg_id = substr(trim($this->headers['message-id']), 1, -1);
-		} else if (isset($this->headers['x-qmail-scanner-message-id'])) {
-			$this->msg_id = substr(trim($this->headers['x-qmail-scanner-message-id']), 1, -1);
-		} else {
-			mlist_error_log('No message id', $this->raw_msg);
-		}
-
-		// This fetches the id of the message if this is a reply to an existing message.
-		if (!empty($this->headers['in-reply-to']) && preg_match('!<([^>]+)>$!', trim($this->headers['in-reply-to']), $match)) {
-			$this->reply_to_msg_id = $match[1];
-		} else if (!empty($this->headers['references']) && preg_match('!<([^>]+)>$!', trim($this->headers['references']), $match)) {
-			$this->reply_to_msg_id = $match[1];
-		}
-	}
-
-	function clean_up_data()
-	{
-		if ($this->subject_cleanup_rgx) {
-			$this->subject = trim(preg_replace($this->subject_cleanup_rgx, $this->subject_cleanup_rep, $this->subject));
-		}
-		if ($this->body_cleanup_rgx) {
-			$this->body = trim(preg_replace($this->body_cleanup_rgx, $this->body_cleanup_rep, $this->body));
-		}
-	}
-}
-
-function mlist_error_log($error, $msg_data, $level='WARNING')
-{
-	$u = umask(0111);
+	// Make copy of message for later investigation.
 	$err_msg_cpy = $GLOBALS['ERROR_PATH'] .'.mlist/'. time() .'_'. md5($msg_data);
-
 	if (!empty($msg_data) && $level != 'LOG') {
+		$u = umask(0111);
 		if (!($fp = fopen($err_msg_cpy, 'wb'))) {
 			exit('No perms to write '. $err_msg_cpy ."\n");
 		}
 		fwrite($fp, $msg_data);
 		fclose($fp);
+		umask($u);
 		$err_msg_cpy = ' @ '. $err_msg_cpy;
 	} else {
 		$err_msg_cpy = '';
 	}
 
+	// Log error message.
 	fud_logerror($error . $err_msg_cpy, 'mlist_errors');
-	umask($u);
 
 	if ($level == 'ERROR') {
 		exit;
@@ -374,7 +84,7 @@ function add_attachment($name, $data, $pid)
 	fud_use('rhost.inc');
 	fud_use('smiley.inc');
 	fud_use('fileio.inc');
-	fud_use('mlist.inc', true);
+	fud_use('mime_decode.inc', true);
 	fud_use('scripts_common.inc', true);
 
 	define('sql_p', $DBHOST_TBL_PREFIX);
@@ -392,10 +102,11 @@ function add_attachment($name, $data, $pid)
 	$FUD_OPT_2 |= $FUD_OPT_2 &~ (1024|8388608);
 	$FUD_OPT_2 |= 128;
 
-	/* Set language & locale. */
+	/* Set language, locale and time zone. */
 	$GLOBALS['usr'] = new stdClass();
 	list($GLOBALS['usr']->lang, $locale) = db_saq('SELECT lang, locale FROM '. sql_p .'themes WHERE theme_opt='. (1|2) .' LIMIT 1');
 	$GLOBALS['good_locale'] = setlocale(LC_ALL, $locale);
+	date_default_timezone_set($GLOBALS['SERVER_TZ']);
 
 	$frm = db_sab('SELECT id, forum_opt, message_threshold, (max_attach_size * 1024) AS max_attach_size, max_file_attachments FROM '. sql_p .'forum WHERE id='. $config->forum_id);
 
@@ -432,7 +143,7 @@ function add_attachment($name, $data, $pid)
 			$flags    = '/ssl/novalidate-cert';
 		}
 
-		// Only use default port if the user haven't done it.
+		// Add default port if the user didn't specify it.
 		$config->mbox_server .= (strpos($config->mbox_server, ':') === FALSE) ? ':'. $port : '';
 
 		// Connect and search for e-mail messages.
@@ -445,7 +156,7 @@ function add_attachment($name, $data, $pid)
 
 	$done = 0;
 	while (!$done) {
-		$emsg = new fud_emsg();
+		$emsg = new fud_mime_msg();
 		$emsg->subject_cleanup_rgx = $config->subject_regex_haystack;
 		$emsg->subject_cleanup_rep = $config->subject_regex_needle;
 		$emsg->body_cleanup_rgx = $config->body_regex_haystack;
@@ -460,14 +171,15 @@ function add_attachment($name, $data, $pid)
 			}
 			$email_number = array_pop($emails);
 			$email_message = imap_fetchbody($mbox, $email_number, '');
-			echo 'Loading message '. $email_number;
-			$emsg->parse_input($config->mlist_opt & 16, $email_message);
-			echo " Done. Deleting message.\n";
+			echo 'Load message '. $email_number;
+			$emsg->parse_message($email_message, $config->mlist_opt & 16);
+			echo ". Done. Delete message.\n";
 			imap_delete($mbox, $email_number);
 		} else {
 			/* Read single message from pipe (stdin) and load into the forum. */
+			$email_message = file_get_contents('php://stdin');
+			$emsg->parse_message($email_message, $config->mlist_opt & 16);
 			$done = 1;
-			$emsg->parse_input($config->mlist_opt & 16);
 		}
 
 		$emsg->fetch_useful_headers();
@@ -483,14 +195,14 @@ function add_attachment($name, $data, $pid)
 		}
 
 		/* Handler for our own messages, which do not need to be imported. */
-		if (isset($emsg->headers['x-fudforum']) && preg_match('!'.md5($GLOBALS['WWW_ROOT']).' <([0-9]+)>!', $emsg->headers['x-fudforum'], $m)) {
+		if (isset($emsg->headers['x-fudforum']) && preg_match('!'. md5($GLOBALS['WWW_ROOT']) .' <([0-9]+)>!', $emsg->headers['x-fudforum'], $m)) {
 			q('UPDATE '. sql_p .'msg SET mlist_msg_id='. _esc($emsg->msg_id) .' WHERE id='. (int)$m[1] .' AND mlist_msg_id IS NULL');
 			continue;
 		}
 
 		$msg_post->post_stamp = !empty($emsg->headers['date']) ? strtotime($emsg->headers['date']) : 0;
 		if ($msg_post->post_stamp < 1 || $msg_post->post_stamp > __request_timestamp__) {
-			mlist_error_log('Invalid Date', $emsg->raw_msg);
+			log_script_error('Invalid date', $emsg->raw_msg);
 			if (($p = strpos($emsg->headers['received'], '; ')) !== false) {
 				$p += 2;
 				$msg_post->post_stamp = strtotime(substr($emsg->headers['received'], $p, (strpos($emsg->headers['received'], '00 ', $p) + 2 - $p)));
@@ -547,7 +259,7 @@ function add_attachment($name, $data, $pid)
 		fud_wordwrap($msg_post->body);
 		$msg_post->subject = htmlspecialchars(apply_custom_replace($emsg->subject));
 		if (!strlen($msg_post->subject)) {
-			mlist_error_log('Blank Subject', $emsg->raw_msg);
+			log_script_error('Blank subject', $emsg->raw_msg);
 			$msg_post->subject = '(no subject)';
 		}
 
@@ -578,6 +290,7 @@ function add_attachment($name, $data, $pid)
 		}
 	}
 
+	// Close the mailbox.
 	if ($config->mbox_server && $config->mbox_user) {
 		@imap_expunge($mbox);
 		@imap_close($mbox);
--- install/forum_data/scripts/nntp.php
+++ install/forum_data/scripts/nntp.php
@@ -10,6 +10,42 @@
 * Free Software Foundation; version 2 of the License. 
 **/
 
+function log_script_error($error, $msg_data='', $level='WARNING')
+{
+	// Make copy of message for later investigation.
+	$err_msg_cpy = $GLOBALS['ERROR_PATH'] .'.nntp/'. time() .'_'. md5($msg_data);
+	if (!empty($msg_data) && $level != 'LOG') {
+		$u = umask(0111);
+		if (!($fp = fopen($err_msg_cpy, 'wb'))) {
+			exit('No perms to write '. $err_msg_cpy ."\n");
+		}
+		fwrite($fp, $msg_data);
+		fclose($fp);
+		umask($u);
+		$err_msg_cpy = ' @ '. $err_msg_cpy;
+	} else {
+		$err_msg_cpy = '';
+	}
+
+	// Log error message.
+	fud_logerror($error . $err_msg_cpy, 'nntp_errors');
+
+	if ($level == 'ERROR') {
+		exit;
+	}
+}
+
+function add_attachment($name, $data, $pid)
+{
+	$tmpfname = tempnam($GLOBALS['TMP'], 'FUDf_');
+	$fp = fopen($tmpfname, 'wb');
+	$len = fwrite($fp, $data);
+	fclose($fp);
+
+	return attach_add(array('name' => basename($name), 'size' => $len, 'tmp_name' => $tmpfname), $pid, 0, 1);
+}
+
+/* main */
 	ini_set('memory_limit', '128M');
 	define('forum_debug', 1);
 	unset($_SERVER['REMOTE_ADDR']);
@@ -55,7 +91,7 @@
 	fud_use('alt_var.inc');
 	fud_use('smiley.inc');
 	fud_use('nntp.inc', true);
-	fud_use('nntp_adm.inc', true);
+	fud_use('mime_decode.inc', true);
 	fud_use('scripts_common.inc', true);
 
 	define('sql_p', $GLOBALS['DBHOST_TBL_PREFIX']);
@@ -68,20 +104,6 @@
 	if (!$config) {
 		exit('Invalid NNTP identifier.');
 	}
-
-	$nntp = new fud_nntp;
-	$nntp->rule_id 		= $config->id;
-	$nntp->server 		= $config->server;
-	$nntp->newsgroup 	= $config->newsgroup;
-	$nntp->port 		= $config->port;
-	$nntp->timeout 		= $config->timeout;
-	$nntp->nntp_opt 	= $config->nntp_opt;
-	$nntp->user 		= $config->login;
-	$nntp->pass 		= $config->pass;
-	$nntp->imp_limit	= $config->imp_limit;
-	$nntp->tracker		= $config->tracker;
-
-	$frm = db_sab('SELECT id, forum_opt, message_threshold, (max_attach_size * 1024) AS max_attach_size, max_file_attachments FROM '. sql_p .'forum WHERE id='. $config->forum_id);
 
 	/* Set language & locale. */
 	$GLOBALS['usr'] = new stdClass();
@@ -97,11 +119,181 @@
 		}
 	}
 
+	/* Fetch forum options. */
+	$frm = db_sab('SELECT id, forum_opt, message_threshold, (max_attach_size * 1024) AS max_attach_size, max_file_attachments FROM '. sql_p .'forum WHERE id='. $config->forum_id);
+		
 	$FUD_OPT_2 |= 128;	// Disable USE_ALIASES.
 
+	$nntp = new fud_nntp;
+	$nntp->server 		= $config->server;
+	$nntp->newsgroup 	= $config->newsgroup;
+	$nntp->port 		= $config->port;
+	$nntp->timeout 		= $config->timeout;
+	$nntp->nntp_opt 	= $config->nntp_opt;
+	$nntp->user 		= $config->login;
+	$nntp->pass 		= $config->pass;
+	
+	// Lock, connect and fetch group_first and group_last (message counters).
 	$lock = $nntp->get_lock();
-	$nntp->parse_msgs($frm, $config, $nntp->tracker);
-	$nntp->release_lock($lock);
+	if (!$nntp->connect()) {
+		$nntp->exit_handler();
+	}
 
+	$nntp->group_last++;
+	if ($config->tracker && $config->tracker > $nntp->group_first) {
+		$nntp->group_first = $config->tracker;
+	}
+
+	$counter = 1;
+
+	for ($i = $nntp->group_first; $i < $nntp->group_last; $i++) {
+		echo 'Importing '. $nntp->newsgroup .' message '. $i ."\n";
+		if (!$nntp->get_message($i)) {
+			$nntp->error = null;
+			continue;
+		}
+
+		$emsg = new fud_mime_msg();
+		$emsg->parse_message($nntp->raw_msg, $frm->forum_opt & 16);
+
+		$emsg->fetch_useful_headers();
+		// $emsg->clean_up_data();	// We may want to add subject & body mangling later.
+
+		$msg_post = new fud_msg_edit;
+
+		// Handler for our own messages, which do not need to be imported.
+		if (isset($emsg->headers['x-fudforum']) && preg_match('!([A-Za-z0-9]{32}) <([0-9]+)>!', $emsg->headers['x-fudforum'], $m)) {
+			if ($m[1] == md5($GLOBALS['WWW_ROOT'])) {
+				q('UPDATE '. sql_p .'msg SET mlist_msg_id='. _esc($emsg->msg_id) .' WHERE id='. intval($m[2]) .' AND mlist_msg_id IS NULL');
+				if (db_affected()) {
+					continue;
+				}
+			}
+		}
+
+		// Handle NNTP cancellation messages.
+		if (isset($emsg->headers['control']) && preg_match('!cancel!', $emsg->headers['control'])) {
+			log_script_error('Ignore NNTP cancellation message (not implemented).', $emsg->raw_msg);
+			// For future implementation. Anyone brave enough to test it for us?
+			// q('DELETE FROM '.sql_p.'msg WHERE mlist_msg_id='._esc($emsg->msg_id));
+			// if (db_affected()) {
+				continue;
+			// }
+		}
+
+		$msg_post->post_stamp = !empty($emsg->headers['date']) ? strtotime($emsg->headers['date']) : 0;
+		if ($msg_post->post_stamp < 1 || $msg_post->post_stamp > __request_timestamp__) {
+			log_script_error('Invalid date', $emsg->raw_msg);
+			if (($p = strpos($emsg->headers['received'], '; ')) !== false) {
+				$p += 2;
+				$msg_post->post_stamp = strtotime(substr($emsg->headers['received'], $p, (strpos($emsg->headers['received'], '00 ', $p) + 2 - $p)));
+			}
+			if ($msg_post->post_stamp < 1 || $msg_post->post_stamp > __request_timestamp__) {
+				$msg_post->post_stamp = __request_timestamp__;
+			}
+		}
+
+		if (!$emsg->from_email || !$emsg->from_name) {
+			$msg_post->poster_id = 0;
+		} else {
+			$msg_post->poster_id = match_user_to_post($emsg->from_email, $emsg->from_name, $config->nntp_opt & 32, $emsg->user_id, $msg_post->post_stamp);
+		}
+
+		/* Check if matching user and if not, skip if necessary. */
+		if (!$msg_post->poster_id && $config->nntp_opt & 256) {
+			continue;
+		}
+
+		$attach_list = array();
+		// Handle NNTP (UUEncoded and Base64) attachments.
+		$msg_post->body = $nntp->parse_attachments($msg_post->body);
+		if (isset($nntp->attachments) && is_array($nntp->attachments)) {
+			foreach($nntp->attachments as $key => $val) {
+				if (!($config->nntp_opt & 8) && (strlen($val) > $frm->max_attach_size || (isset($attach_list) && count($attach_list) > $frm->max_file_attachments) || filter_ext($key))) {
+					continue;
+				}
+				$id = add_attachment($key, $val, $msg_post->poster_id);
+				$attach_list[$id] = $id;
+				unset($nntp->attachments[$k]);
+			}
+		}
+
+		/* Handle inlined attachments. */
+		if ($config->nntp_opt & 8) {
+			foreach ($emsg->inline_files as $key => $val) {
+				if (strpos($emsg->body, 'cid:'. $val) !== false) {
+					$id = add_attachment($key, $emsg->attachments[$key], $msg_post->poster_id);
+					$attach_list[$id] = $id;
+					$emsg->body = str_replace('cid:'. $val, $WWW_ROOT .'index.php?t=getfile&amp;id='. $id, $emsg->body);
+				}
+				unset($emsg->attachments[$key]);
+			}
+		}
+
+		$msg_post->body = trim($emsg->body);
+
+		/* For anonymous users prefix 'contact' link. */
+		if (!$msg_post->poster_id) {
+			if ($frm->forum_opt & 16) {
+				$msg_post->body = '[b]Originally posted by:[/b] [email='. $emsg->from_email .']'. (!empty($emsg->from_name) ? $emsg->from_name : $emsg->from_email) ."[/email]\n\n". $msg_post->body;
+			} else {
+				$msg_post->body = 'Originally posted by: '. str_replace('@', '&#64', $emsg->from_email) ."\n\n". $msg_post->body;
+			}
+		}
+
+		$msg_post->body = apply_custom_replace($msg_post->body);
+		if ($frm->forum_opt & 16) {
+			$msg_post->body = tags_to_html($msg_post->body, 0);
+		} else {
+			$msg_post->body = nl2br($msg_post->body);
+		}
+
+		fud_wordwrap($msg_post->body);
+		$msg_post->subject = htmlspecialchars(apply_custom_replace($emsg->subject));
+		if (!strlen($msg_post->subject)) {
+			log_script_error('Blank subject', $emsg->raw_msg);
+			$msg_post->subject = '(no subject)';
+		}
+
+		$msg_post->ip_addr = $emsg->ip;
+		$msg_post->mlist_msg_id = $emsg->msg_id;
+		$msg_post->attach_cnt = 0;
+		$msg_post->poll_id = 0;
+		$msg_post->msg_opt = 1|2;
+
+		// Try to determine whether this message is a reply or a new thread.
+		list($msg_post->reply_to, $msg_post->thread_id) = get_fud_reply_id(($config->nntp_opt & 16), $frm->id, $msg_post->subject, $emsg->reply_to_msg_id);
+
+		$msg_post->add($frm->id, $frm->message_threshold, 0, 0, false);
+
+		// Handle file attachments.
+		if ($config->nntp_opt & 8) {
+			foreach($emsg->attachments as $key => $val) {
+				$id = add_attachment($key, $val, $msg_post->poster_id);			
+				$attach_list[$id] = $id;
+			}
+		}
+		if ($attach_list) {
+			attach_finalize($attach_list, $msg_post->id);
+		}
+
+		if (!($config->nntp_opt & 1)) {
+			fud_msg_edit::approve($msg_post->id);
+		}
+
+		unset($emsg);
+		unset($msg_post);
+
+		/* Message import limit reached. */
+		if ($config->imp_limit && $counter++ >= $config->imp_limit) {
+			break;
+		}
+	}
+
+	// Store current position.
+	$nntp->set_tracker_end($config->id, $i); // We use $i so we stop in the right place if limit is reached.
+ 	$nntp->exit_handler();
+
+	$nntp->release_lock($lock);
 	$nntp->close_connection();
 ?>
