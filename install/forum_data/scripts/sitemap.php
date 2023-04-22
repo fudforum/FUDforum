86de6180c03e76a366af820adb06cb62cc7744e7
naudefj <652259+naudefj@users.noreply.github.com>
2023-04-22 14:13:29 +0200

    Fix PHP warnings

--- install/forum_data/scripts/sitemap.php
+++ install/forum_data/scripts/sitemap.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php -q
 <?php
 /**
-* copyright            : (C) 2001-2011 Advanced Internet Designs Inc.
+* copyright            : (C) 2001-2023 Advanced Internet Designs Inc.
 * email                : forum@prohost.org
 * $Id$
 *
@@ -40,7 +40,7 @@
 
 	$c = uq(q_limit('SELECT t.id, t.last_post_date, m.subject FROM '. $GLOBALS['DBHOST_TBL_PREFIX'] .'thread t '. $join .'INNER JOIN '. $GLOBALS['DBHOST_TBL_PREFIX'] .'msg m ON t.root_msg_id = m.id WHERE '. $lmt .' ORDER BY t.last_post_date DESC', 50000));
 
-	echo "Writing sitemap.xml file to ${GLOBALS['WWW_ROOT_DISK']}\n";
+	echo "Writing sitemap.xml file to {$GLOBALS['WWW_ROOT_DISK']}\n";
 	$fh = fopen($GLOBALS['WWW_ROOT_DISK'].'/sitemap.xml', 'w');
 	$xmlhead = <<<EOF
 <?xml version='1.0' encoding='UTF-8'?>
@@ -60,11 +60,11 @@ EOF;
 			$thread_title_SEO = preg_replace(array('/[^a-z0-9_]/', '/_[_]*/', '/-[-]*/'), '-', strtolower($r[2]));
 			$thread_title_SEO = str_replace(array('-s-', '%'), array('s-', ''), $thread_title_SEO);
 
-			$filetext .= "\t<loc>${WWW_ROOT}index.php/t/${thread_id}/${thread_title_SEO}</loc>\n";			
+			$filetext .= "\t<loc>{$WWW_ROOT}index.php/t/{$thread_id}/{$thread_title_SEO}</loc>\n";
 		} else {
-			$filetext .= "\t<loc>${WWW_ROOT}index.php?t=msg&amp;th=${thread_id}&amp;start=0</loc>\n";
+			$filetext .= "\t<loc>{$WWW_ROOT}index.php?t=msg&amp;th={$thread_id}&amp;start=0</loc>\n";
 		}
-		$filetext .= "\t<lastmod>${post_stamp}+00:00</lastmod>\n";
+		$filetext .= "\t<lastmod>{$post_stamp}+00:00</lastmod>\n";
 		$filetext .= "\t<changefreq>$frequency</changefreq>\n";
 		$filetext .= "\t<priority>$priority</priority>\n";
 		$filetext .= "</url>\n";
